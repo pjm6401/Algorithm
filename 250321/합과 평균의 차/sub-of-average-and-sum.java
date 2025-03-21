fd36de9e2b27ba5d177a33631e77a9f8f8d1d142
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-03-21 16:33:37 +0900

    Title: 합과 평균의 차, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 250321/합과 평균의 차/sub-of-average-and-sum.java
+++ 250321/합과 평균의 차/sub-of-average-and-sum.java
@@ -5,12 +5,13 @@ public class Main {
 
         int a =sc.nextInt();
         int b =sc.nextInt();
-        int b =sc.nextInt();
+        int c =sc.nextInt();
 
         int sum = (a+b+c);
         int mean = (a+b+c)/3;
 
-        System.out.println();
-        System.out.println();
+        System.out.println(sum);
+        System.out.println(mean);
+        System.out.println(sum-mean);
     }
 }
