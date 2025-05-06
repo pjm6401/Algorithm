546e08a6c4a8bd64bf344da86977e402e0d388ad
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-06 19:03:07 +0900

    Title: 코로나 메뉴얼2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250506/코로나 메뉴얼2/covid-manual2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 예시 입력: 23 42
+        String[] input = br.readLine().split(" ");
+        int firstState = input[0];
+        int firstTemp = Integer.parseInt(input[1]);
+        int firstState = input[0];
+        int firstTemp = Integer.parseInt(input[1]);
+    }
+}
