5a4a37a16cb616ab46b44a812dcef268133f75bf
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-06 19:16:04 +0900

    Title: 코로나 메뉴얼2, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

--- 250506/코로나 메뉴얼2/covid-manual2.java
+++ 250506/코로나 메뉴얼2/covid-manual2.java
@@ -4,11 +4,38 @@ public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 예시 입력: 23 42
-        String[] input = br.readLine().split(" ");
-        int firstState = input[0];
-        int firstTemp = Integer.parseInt(input[1]);
-        int firstState = input[0];
-        int firstTemp = Integer.parseInt(input[1]);
+        String state[] = new String [3];
+        int temp [] =new int [3];
+        for (int i = 0; i < 3; i++) {
+            String[] input = br.readLine().split(" ");
+            state[i] = input[0];         // 'Y' 또는 'N'
+            temp[i] = Integer.parseInt(input[1]); // 38, 36, 40 같은 숫자
+        }
+
+        int hospital[] = {0,0,0,0};
+
+
+        for(int i =0; i<3; i++){
+
+            if(state[i].equals("Y") &&  temp[i]>=37){
+                hospital[0] +=1;
+            }
+            else if(state[i].equals("N") &&  temp[i]>=37){
+                hospital[1] +=1;
+            }
+            else if(state[i].equals("Y") &&  temp[i]<37){
+                hospital[2] +=1;
+            }
+            else{
+                hospital[3] +=1;
+            }
+        }
+
+        for(int i =0; i<hospital.length; i++){
+            System.out.print(hospital[i]+" ");
+        }
+        if(hospital[0]>=2){
+            System.out.print("E");
+        }
     }
 }
