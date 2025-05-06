2c14fbdb471378c04516c1f25e15634950764cf2
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-06 20:17:33 +0900

    Title: 연속부분수열일까, Time: 97ms, Memory: 9MB, Status: Passed - Codetree

--- 250506/연속부분수열일까/contiguous-array-or-not.java
+++ 250506/연속부분수열일까/contiguous-array-or-not.java
@@ -23,16 +23,24 @@ public class Main  {
         }
 
         String flag = "No";
-        for(int i =0; i<n1-n2;i++){
+        for(int i =0; i<=n1-n2;i++){
             for(int j=0;j<n2;j++){
                 if(arrayN1[j+i] == arrayN2[j]){
-                    
+                    /*System.out.println("i : "+i);
+                    System.out.println("arrayN1 : "+arrayN1[j+i]);
+                    System.out.println("arrayN2 : "+arrayN2[j]);
+                    System.out.println("j : "+j);*/
                     flag = "Yes";
                 }else{
+                    /*System.out.println("i : "+i);
+                    System.out.println("arrayN1 : "+arrayN1[j+i]);
+                    System.out.println("arrayN2 : "+arrayN2[j]);
+                    System.out.println("j : "+j);*/
                     flag = "No";
                     break;
                 }
             }
+            //System.out.println("Result: "+ flag);
             if(flag.equals("Yes")){
                 break;
             }            
