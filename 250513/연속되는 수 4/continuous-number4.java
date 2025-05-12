3c0368fa3dacc4eb4829bec9b76d485ce89d85fc
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-13 00:13:56 +0900

    Title: 연속되는 수 4, Time: 117ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250513/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int cnt =0;
+        int max =0;
+
+        for(int i =1; i<n; i++){
+            if(isPlus(arr[i],arr[i-1])){
+                cnt++;
+            }else{
+                if(cnt>max){
+                    max = cnt;
+                }
+            }
+        }
+
+        if(max ==0){
+            max = cnt;
+        }
+        System.out.println(max);
+    }
+
+
+    public static boolean isPlus(int x, int y){
+        return x>y;
+    }
+}
