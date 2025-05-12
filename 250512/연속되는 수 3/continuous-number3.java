6da2589231420cb4e9d4baddc819bf92ee732bb4
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-12 23:47:22 +0900

    Title: 연속되는 수 3, Time: 169ms, Memory: 11MB, Status: Passed - Codetree

--- 250512/연속되는 수 3/continuous-number3.java
+++ 250512/연속되는 수 3/continuous-number3.java
@@ -5,17 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
-        
         int cnt = 0;
         int max = 0;
-        for(int i = 1; i<n; i++){
-            if(isPositive(arr[i]) && isPositive(arr[i-1])){
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            if(i!=0 && isPositive(arr[i]) && isPositive(arr[i-1])){
                 cnt++;
-            }else if (!isPositive(arr[i]) && !isPositive(arr[i-1])){
+            }else if (i!=0 && !isPositive(arr[i]) && !isPositive(arr[i-1])){
                 cnt++;
-            }else{
+            }else if (i!=0) {
                 if(cnt+1>max){
                     max = cnt+1;
                 }
