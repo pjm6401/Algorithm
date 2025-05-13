07878ebfabc0f3f39ad6f7ee15bf3ca6bd26ac99
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-13 23:56:26 +0900

    Title: 만나는 그 순간, Time: 128ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250513/만나는 그 순간/the-moment-we-meet.java
+++ 250513/만나는 그 순간/the-moment-we-meet.java
@@ -3,79 +3,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int n = sc.nextInt();
         int m = sc.nextInt();
         
-        int [] arrA = new int[1000];
-        int [] arrB = new int[1000];
-        int aPoint = 0;
-        int bPoint = 0;
-        int aIndex = 0;
-        int bIndex = 0;
+        int[] arrA = getPath(sc, n);
+        int[] arrB = getPath(sc, m);
 
-        int aLength = 1;
-        int bLength = 1;
-        arrA[aIndex] =aPoint;
-        arrB[bIndex] =bPoint;
- 
-        
-        for(int i =0; i<n; i++){
-            char direction = sc.next().charAt(0);
-            int distance = sc.nextInt();
-            aLength = aLength+distance;
-            for(int j =0; j<distance; j++){
-                aIndex += 1;
-                if(direction == 'R'){
-                    aPoint += 1;
-                    arrA[aIndex] = aPoint;
-                    
-                }else{
-                    aPoint -= 1;
-                    arrA[aIndex] = aPoint;
-                    
-                }
-            }
-        }
-
-        for(int i =0; i<m; i++){
-            char direction = sc.next().charAt(0);
-            int distance = sc.nextInt();
-            bLength = bLength+distance;
-            for(int j =0; j<distance; j++){
-                bIndex += 1;
-                if(direction == 'R'){
-                    bPoint += 1;
-                    arrB[bIndex] = bPoint;
-                    
-                }else{
-                    bPoint = bPoint-1;
-                    arrB[bIndex] = bPoint;
-                    
-                }
-            }
-        }
-
-
-        int length = 0;
-
-        if(aLength>bLength){
-            length = bLength;
-        }else{
-            length = aLength;
-        }
-
-
+        int minLength = Math.min(arrA.length, arrB.length);
         int meetPoint = -1;
 
-        for (int i =1; i<length; i++){
-            if(arrA[i] == arrB[i]){
+        for (int i = 1; i < minLength; i++) {
+            if (arrA[i] == arrB[i]) {
                 meetPoint = i;
                 break;
             }
         }
 
         System.out.println(meetPoint);
-        // Please write your code here.
-        
+    }
+
+    private static int[] getPath(Scanner sc, int moves) {
+        int[] path = new int[1000];
+        int position = 0;
+        int index = 0;
+        path[index] = position;
+
+        for (int i = 0; i < moves; i++) {
+            char direction = sc.next().charAt(0);
+            int distance = sc.nextInt();
+
+            for (int j = 0; j < distance; j++) {
+                position += direction == 'R' ? 1 : -1;
+                path[++index] = position;
+            }
+        }
+
+        // 배열의 실제 길이만큼 복사해서 반환
+        int[] result = new int[index + 1];
+        System.arraycopy(path, 0, result, 0, index + 1);
+        return result;
     }
 }
