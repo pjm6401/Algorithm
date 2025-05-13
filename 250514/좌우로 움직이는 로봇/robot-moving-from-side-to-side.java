0c50ecc70c735f966c4bcde4c9ab6b3d3616bb81
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-14 03:36:35 +0900

    Title: 좌우로 움직이는 로봇, Time: 1767ms, Memory: 90MB, Status: Passed - Codetree

--- 250514/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250514/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -9,61 +9,46 @@ public class Main {
         aList.add(0);
         List<Integer> bList = new ArrayList<>();
         bList.add(0);
+        // A 이동 경로 기록
         for (int i = 0; i < n; i++) {
             int t = sc.nextInt();
             char d = sc.next().charAt(0);
-            for(int j = 0; j<t;j++){
-                if(d == 'L'){
-                    aList.add(aList.get(aList.size()-1)-1);
-                }
-                if(d == 'R'){
-                    aList.add(aList.get(aList.size()-1)+1);
-                }
+            int dir = (d == 'L') ? -1 : 1;
+            for (int j = 0; j < t; j++) {
+                int prev = aList.get(aList.size() - 1);
+                aList.add(prev + dir);
             }
-            
         }
-        
+
+        // B 이동 경로 기록
         for (int i = 0; i < m; i++) {
             int t = sc.nextInt();
             char d = sc.next().charAt(0);
-            for(int j = 0; j<t;j++){
-                 if(d == 'L'){
-                    bList.add(bList.get(bList.size()-1)-1);
-                }
-                if(d == 'R'){
-                    bList.add(bList.get(bList.size()-1)+1);
-                }
+            int dir = (d == 'L') ? -1 : 1;
+            for (int j = 0; j < t; j++) {
+                int prev = bList.get(bList.size() - 1);
+                bList.add(prev + dir);
             }
         }
 
-        int length = Math.max(aList.size(),bList.size());
-        int cnt =0;
-        int location = 0;
-        for(int i =1; i<length; i++){
-            if(aList.size()<length && !(aList.size()>i)){
-                int target = aList.get(aList.size()-1);
-
-                if(bList.get(i) == target && bList.get(i-1) != target){
-                    cnt++;
-                    location = target;
-                }
-
-            }else if(bList.size()<length && !(bList.size()>i)){
-                int target = bList.get(bList.size()-1);
-                if(aList.get(i) == target && aList.get(i-1) != target){
-                    cnt++;
-                    location = target;
-                } 
-            }else{
-                if(aList.get(i) == bList.get(i) && aList.get(i-1) != bList.get(i-1)){
-                    cnt ++;
-                    location = aList.get(i);
-                }
-            }
-            
+        // 길이를 맞추기 위해 마지막 위치를 반복해서 추가
+        int maxLen = Math.max(aList.size(), bList.size());
+        while (aList.size() < maxLen) {
+            aList.add(aList.get(aList.size() - 1)); // A가 멈춰있는 위치
         }
-        
-        System.out.println(cnt);
+        while (bList.size() < maxLen) {
+            bList.add(bList.get(bList.size() - 1)); // B가 멈춰있는 위치
+        }
+        int count = 0;
+        for (int i = 1; i < maxLen; i++) {
+            // 이전 위치는 다르고, 현재 위치는 같다면 카운트
+            if (!aList.get(i - 1).equals(bList.get(i - 1)) &&
+                aList.get(i).equals(bList.get(i))) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
         // Please write your code here.
     }
 }
