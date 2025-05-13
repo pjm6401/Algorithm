cc078698ea9d25737cd1a326d43d8cde746cb075
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-14 06:15:24 +0900

    Title: 좌우로 움직이는 로봇, Time: 1013ms, Memory: 58MB, Status: Passed - Codetree

--- 250514/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250514/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -1,54 +1,74 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_T = 1000000;
+    
+    public static int n, m;
+    public static int[] posA = new int[MAX_T  + 1];
+    public static int[] posB = new int[MAX_T  + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        List<Integer> aList = new ArrayList<>();
-        aList.add(0);
-        List<Integer> bList = new ArrayList<>();
-        bList.add(0);
-        // A 이동 경로 기록
-        for (int i = 0; i < n; i++) {
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+        
+        // A가 매 초마다 서있는 위치를 기록
+        int timeA = 1;
+        for(int i = 0; i < n; i++) {
             int t = sc.nextInt();
             char d = sc.next().charAt(0);
-            int dir = (d == 'L') ? -1 : 1;
-            for (int j = 0; j < t; j++) {
-                int prev = aList.get(aList.size() - 1);
-                aList.add(prev + dir);
+            
+            while(t-- > 0) {
+                if(d == 'R')
+                    posA[timeA] = posA[timeA - 1] + 1;
+                else
+                    posA[timeA] = posA[timeA - 1] - 1;
+                
+                timeA++;
             }
         }
-
-        // B 이동 경로 기록
-        for (int i = 0; i < m; i++) {
+        
+        // B가 매 초마다 서있는 위치를 기록
+        int timeB = 1;
+        for(int i = 0; i < m; i++) {
             int t = sc.nextInt();
             char d = sc.next().charAt(0);
-            int dir = (d == 'L') ? -1 : 1;
-            for (int j = 0; j < t; j++) {
-                int prev = bList.get(bList.size() - 1);
-                bList.add(prev + dir);
+            
+            while(t-- > 0) {
+                if(d == 'R')
+                    posB[timeB] = posB[timeB - 1] + 1;
+                else
+                    posB[timeB] = posB[timeB - 1] - 1;
+                
+                timeB++;
             }
         }
-
-        // 길이를 맞추기 위해 마지막 위치를 반복해서 추가
-        int maxLen = Math.max(aList.size(), bList.size());
-        while (aList.size() < maxLen) {
-            aList.add(aList.get(aList.size() - 1)); // A가 멈춰있는 위치
-        }
-        while (bList.size() < maxLen) {
-            bList.add(bList.get(bList.size() - 1)); // B가 멈춰있는 위치
-        }
-        int count = 0;
-        for (int i = 1; i < maxLen; i++) {
-            // 이전 위치는 다르고, 현재 위치는 같다면 카운트
-            if (!aList.get(i - 1).equals(bList.get(i - 1)) &&
-                aList.get(i).equals(bList.get(i))) {
-                count++;
+        
+        if(timeA < timeB) {
+            for(int i = timeA; i < timeB; i++) {
+                posA[i] = posA[i - 1];
             }
         }
-
-        System.out.println(count);
-        // Please write your code here.
+        else if(timeA > timeB) {
+            for(int i = timeB; i < timeA; i++) {
+                posB[i] = posB[i - 1];
+            }
+        }
+        
+        // 새롭게 만나는 횟수를 구합니다.
+        int cnt = 0;
+        int timeMax = 0;
+        if(timeA < timeB)
+            timeMax = timeB;
+        else
+            timeMax = timeA;
+        
+        for(int i = 1; i < timeMax; i++) {
+            if(posA[i] == posB[i] && posA[i - 1] != posB[i - 1])
+                cnt++;
+        }
+        
+        System.out.print(cnt);
     }
 }
