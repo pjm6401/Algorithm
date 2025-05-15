e8365bfd137e696c36276faab9d73054632b6906
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-16 06:17:09 +0900

    Title: 거울에 레이저 쏘기 2, Time: 131ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250516/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int [] dx = {0,1,0,-1};
+    static int [] dy = {1,0,-1,0};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[][] grid = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = s.charAt(j);
+            }
+        }
+        int startNum = sc.nextInt();
+        // Please write your code here.
+
+        
+        int [] points = startPoint(n,startNum);
+        boolean flag = true;
+        int count = 0;
+        while(true){
+            count ++;
+            int x = points[0];
+            int y = points[1];
+            int direction = points[2];
+            char mirror = grid[x][y];
+            
+            points = nextPoint(x,y,direction,mirror);
+            
+            if(!isEnd(n,points[0],points[1])){
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+    // 빛의 진행 방향을 나타냄 ex) D = 위에서 아래로
+    public static int [] nextPoint(int x, int y, int direction, char mirror){
+
+        int [] points = new int[3];
+
+        if(mirror == '/'){
+            if(direction == 0){
+                direction = 3;
+                return new int[]{x+dx[direction],y+dy[direction],direction};
+            }else if(direction == 1){
+                direction = 2;
+                return new int[]{x+dx[direction],y+dy[direction],direction};
+            }else if(direction == 2){
+                direction = 1;
+                return new int[]{x+dx[direction],y+dy[direction],direction};
+            }else{
+                direction = 0;
+                return new int[]{x+dx[direction],y+dy[direction],direction};
+            }
+        }else{
+            if(direction == 0){
+                direction = 1;
+                return new int[]{x+dx[direction],y+dy[direction],direction};
+            }else if(direction == 1){
+                direction = 0;
+                return new int[]{x+dx[direction],y+dy[direction],direction};
+            }else if(direction == 2){
+                direction = 3;
+                return new int[]{x+dx[direction],y+dy[direction],direction};
+            }else{
+                direction = 2;
+                return new int[]{x+dx[direction],y+dy[direction],direction};
+            }
+        }
+    }
+
+
+    public static boolean isEnd(int n,int x, int y){
+        return (x>=0 && x<n && y>=0 && y<n);
+    } 
+
+    public static int [] startPoint(int n,int start){
+
+        int [] points = new int[3];
+        start --;
+        if(start<n){
+            return new int[]{0,start,1};
+        } else if(start<2*n){
+            return new int[]{start-n,n-1,2};
+        } else if (start<3*n){
+            return new int[]{n-1,3*n-start-1,3};
+        }else {
+            return new int[]{4*n-start-1,0,0};
+        }
+    }
+}
