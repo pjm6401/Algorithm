40bcb20c5e9244cf63662a518b71543a71b165c1
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-18 06:01:07 +0900

    Title: 이상한 수열, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250518/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(result(n));
+    }
+
+    public static int result(int n){
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        return result(n/3) + result(n-1);
+    }
+}
