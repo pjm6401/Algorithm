ddc127b67b63ef40a2764a02beca5e7e1d41ef9c
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-18 06:17:56 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250518/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        System.out.println(result(n-1,arr));
+    }
+
+    public static int result (int n,int [] arr){
+        if(n==0){
+            return arr[0];
+        }
+        
+        
+        if(arr[n]%arr[n-1] ==0 || arr[n-1]%arr[n] ==0){
+            return Math.min(arr[n],arr[n-1])* result(n-1,arr);
+        }else{
+            return arr[n-1]* result(n-1,arr);
+        }
+    }
+}
