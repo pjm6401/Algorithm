07072e966bce4d6e3e157ee61988a3b37dde50ee
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-18 04:36:59 +0900

    Title: 그 계절, 그 날, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250518/그 계절, 그 날/that-season-that-day.java
+++ 250518/그 계절, 그 날/that-season-that-day.java
@@ -1,59 +1,49 @@
 import java.util.Scanner;
+
 public class Main {
 
-    static int [] month = {31,0,31,30,31,30,31,31,30,31,30,31};
+    static int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
         int m = sc.nextInt();
         int d = sc.nextInt();
-        System.out.println(isDate(y,m,d));
+        System.out.println(getSeasonOrInvalid(y, m, d));
     }
 
-    public static String isDate(int y, int m, int d){
-        boolean flag = false;
-
-        if(y%4==0 && y%100 !=0 || (y%4==0 && y%100 ==0 && y%400 ==0)){
-            flag = true;;
-        }
-
-        if(flag){
-            if(m==2){
-                if(d>29){
-                    return "-1";
-                }else{
-                    return "Winter";
-                }     
-            }else{
-                return season(m,d);
-            }
-        }else{
-            if(m==2){
-                if(d>28){
-                    return "-1";
-                }else{
-                    return "Winter";
-                }     
-            }else{
-                return season(m,d);
-            }
-        }
-
-    }
-
-    public static String season(int m,int d){
-        if((2<m && m<=5)&& month[m-1]>=d){
-            return "Spring";
-        }else if((5<m && m<=8)&& month[m-1]>=d){
-            return "Summer";
-        }else if((8<m && m<=11)&& month[m-1]>=d){
-            return "Fall";
-        }else if(((11<m && m<=12) || (0<m && m<=2)) && month[m-1]>=d){
-            return "Winter";
-        }else{
+    public static String getSeasonOrInvalid(int year, int month, int day) {
+        if (month < 1 || month > 12 || day < 1) {
             return "-1";
         }
 
+        int maxDay = daysInMonth[month - 1];
+
+        // 윤년이면 2월 29일까지 허용
+        if (isLeapYear(year) && month == 2) {
+            maxDay = 29;
+        }
+
+        if (day > maxDay) {
+            return "-1";
+        }
+
+        return getSeason(month);
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static String getSeason(int month) {
+        if (month >= 3 && month <= 5) {
+            return "Spring";
+        } else if (month >= 6 && month <= 8) {
+            return "Summer";
+        } else if (month >= 9 && month <= 11) {
+            return "Fall";
+        } else { // 12, 1, 2
+            return "Winter";
+        }
     }
 }
