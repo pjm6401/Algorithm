ad8fdb456a0d69e21a18da480437cdb37bf27e58
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-18 06:19:57 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 110ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250518/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
+++ 250518/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -15,6 +15,13 @@ public class Main {
         if(n==0){
             return arr[0];
         }
+        if(n==1){
+            if(arr[n]%arr[n-1] ==0 || arr[n-1]%arr[n] ==0){
+                return Math.min(arr[n],arr[n-1]);
+            }else{
+                return arr[1] * arr[0];
+            }
+        }
         
         
         if(arr[n]%arr[n-1] ==0 || arr[n-1]%arr[n] ==0){
