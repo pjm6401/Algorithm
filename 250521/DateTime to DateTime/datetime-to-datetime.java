3ab719db65ffbea8d2ec025db69e7b2b3c1ee387
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-21 01:45:53 +0900

    Title: DateTime to DateTime, Time: 114ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250521/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        
+        if(isInBound(A,B,C)){
+            int startMin = (11*24*60)+(11*60)+11;
+            int endMin = (A*24*60)+(B*60)+C;
+
+            System.out.println(endMin-startMin);
+        }else{
+            System.out.println(-1);
+        }
+        
+
+    }
+
+    public static boolean isInBound(int A, int B, int C){
+        return (A>=11 && B>=11 && C>=11);
+
+    }
+}
