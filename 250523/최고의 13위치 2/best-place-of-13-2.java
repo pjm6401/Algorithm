0ada689bc4cfee3f51065f3392aec9247e78e95b
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-23 03:41:29 +0900

    Title: 최고의 13위치 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250523/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                arr[i][j] = sc.nextInt();
+        // Please write your code here.
+        int max = 0;
+         for(int i = 0; i < n-1; i++)
+            for(int j = 0; j < n-2; j++)
+                for(int k = i+1; i < n; i++)
+                    for(int l = 0; l < n-2; l++){
+                        if(max<arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[k][l]+arr[k][l+1]+arr[k][l+2]){
+                            max = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[k][l]+arr[k][l+1]+arr[k][l+2];
+                        }
+                    }
+                        
+
+        System.out.print(max);
+                
+    }
+}
