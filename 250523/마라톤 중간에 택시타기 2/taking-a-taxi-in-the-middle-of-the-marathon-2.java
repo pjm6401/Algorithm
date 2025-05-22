a7839f7cbbc482ca4196d0586b9a1dd639f0fb88
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-23 03:02:55 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250523/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+        int index = 1;
+        int distance = 0;
+        int xPoint = x[0];
+        int yPoint = y[0];
+        for (int i = 1; i < n-1; i++) {
+            for(int j = 1; j<n;j++){
+                if(j==index) continue;
+                int distanceX = Math.abs(xPoint-x[j]);
+                int distanceY = Math.abs(yPoint-y[j]);
+                distance = distance + distanceX + distanceY;
+                xPoint = x[j];
+                yPoint = y[j];
+            }   
+            index++;
+            min = Math.min(min,distance);
+            xPoint = x[0];
+            yPoint = y[0];
+        }
+        System.out.println(min);
+    }
+
+}
