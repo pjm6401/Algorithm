10d2bcfd4912dab1fac8075b956c64a0aa1de550
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-25 08:27:34 +0900

    Title: 특정 구간의 원소 평균값, Time: 162ms, Memory: 10MB, Status: Passed - Codetree

--- 250525/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
+++ 250525/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,31 +6,25 @@ public class Main {
         int[] arr = new int[n];
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
-
+        
         int cnt = 0;
 
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += arr[j];
-                int len = j - i + 1;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int sum = 0;
+                for(int k = i; k <= j; k++)
+                    sum += arr[k];
 
-                // 평균이 정수인지 확인
-                if (sum % len != 0)
-                    continue;
-
-                int mean = sum / len;
-
-                // 해당 구간 안에 평균값이 존재하는지 확인
-                for (int k = i; k <= j; k++) {
-                    if (arr[k] == mean) {
-                        cnt++;
+                double avg = (double)sum / (j - i + 1);
+                for(int k = i; k <= j; k++){
+                    if(arr[k] == avg){
+                        cnt ++;
                         break;
                     }
+                        
                 }
             }
         }
-
         System.out.println(cnt);
     }
 }
