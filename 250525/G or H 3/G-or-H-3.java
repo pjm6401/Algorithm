c89702e7fa22fcddfb0253d51d0f1bf3b5ee51c3
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-25 08:10:29 +0900

    Title: G or H 3, Time: 186ms, Memory: 10MB, Status: Passed - Codetree

--- 250525/G or H 3/G-or-H-3.java
+++ 250525/G or H 3/G-or-H-3.java
@@ -5,7 +5,7 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        char [] arr = new char [10011];
+        char [] arr = new char [10001];
 
         for (int i = 0; i < n; i++) {
             int pos = sc.nextInt();
@@ -14,7 +14,7 @@ public class Main {
             arr[pos-1] = c;
         }
         int max = 0;
-        for (int i = 0; i < 10000-k+2; i++) {
+        for (int i = 0; i < 10000-k; i++) {
             int sum = 0;
             for(int j = i; j <= i+k; j++){
                 int num = 0;
