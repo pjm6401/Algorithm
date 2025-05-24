8dde9e4bfd99ce6faf7b4792a8c17f09a98b0011
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-25 08:09:38 +0900

    Title: G or H 3, Time: 196ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250525/G or H 3/G-or-H-3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        char [] arr = new char [10011];
+
+        for (int i = 0; i < n; i++) {
+            int pos = sc.nextInt();
+            char c = sc.next().charAt(0);
+
+            arr[pos-1] = c;
+        }
+        int max = 0;
+        for (int i = 0; i < 10000-k+2; i++) {
+            int sum = 0;
+            for(int j = i; j <= i+k; j++){
+                int num = 0;
+                if(arr[j] == 'G') num = 1;
+                else if(arr[j] == 'H') num = 2;
+
+                sum += num;
+            }
+            max = Math.max(max,sum);
+        }
+        System.out.print(max);
+    }
+}
