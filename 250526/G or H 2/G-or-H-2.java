fe7ff2b31e7fcc45c07f635edcf8529eca62ed75
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-26 04:01:11 +0900

    Title: G or H 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250526/G or H 2/G-or-H-2.java
+++ 250526/G or H 2/G-or-H-2.java
@@ -32,57 +32,40 @@ public class Main {
             return;
         }
 
-        User[] users = new User[n];
-
-        for (int i = 0; i < n; i++) {
-            int position = sc.nextInt();
-            char ch = sc.next().charAt(0);
-            users[i] = new User(position, ch);
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            char c = sc.next().charAt(0);
+            
+            if(c == 'G')
+                arr[x] = 1;
+            else
+                arr[x] = 2;
         }
 
-        Arrays.sort(users); // 위치 기준 정렬
-
-        int[] gPrefix = new int[n + 1]; // i번째까지 G의 누적 수
-        int[] hPrefix = new int[n + 1]; // i번째까지 H의 누적 수
-
-        for (int i = 1; i <= n; i++) {
-            gPrefix[i] = gPrefix[i - 1];
-            hPrefix[i] = hPrefix[i - 1];
-            if (users[i - 1].getCh() == 'G') gPrefix[i]++;
-            else hPrefix[i]++;
-        }
-
-        int max = 0;
-
-        // 1. G만 있는 구간, H만 있는 구간
-        int gStart = -1;
-        int hStart = -1;
-        for (int i = 0; i < n; i++) {
-            if (users[i].getCh() == 'G') {
-                if (gStart == -1) gStart = i;
-                max = Math.max(max, users[i].getPosition() - users[gStart].getPosition());
-            } else gStart = -1;
-
-            if (users[i].getCh() == 'H') {
-                if (hStart == -1) hStart = i;
-                max = Math.max(max, users[i].getPosition() - users[hStart].getPosition());
-            } else hStart = -1;
-        }
-
-        // 2. G와 H가 같은 개수인 구간
-        Map<Integer, Integer> diffMap = new HashMap<>(); // key: gCount - hCount, value: 처음 나온 인덱스
-        diffMap.put(0, 0); // 초기값 (g-h = 0일 때 위치 0)
-
-        for (int i = 1; i <= n; i++) {
-            int diff = gPrefix[i] - hPrefix[i];
-            if (diffMap.containsKey(diff)) {
-                int prevIndex = diffMap.get(diff);
-                max = Math.max(max, users[i - 1].getPosition() - users[prevIndex].getPosition());
-            } else {
-                diffMap.put(diff, i);
+        int maxLen = 0;
+        for(int i = 0; i <= MAX_NUM; i++) {
+            for(int j = i + 1; j <= MAX_NUM; j++) {
+                if(arr[i] == 0 || arr[j] == 0)
+                    continue;
+                
+                int cntG = 0;
+                int cntH = 0;
+                
+                for(int k = i; k <= j; k++) {
+                    if(arr[k] == 1)
+                        cntG++;
+                    if(arr[k] == 2)
+                        cntH++;
+                }
+                
+                // 조건을 만족할 때 구간의 길이를 구해 최댓값과 비교합니다.
+                if(cntG == 0 || cntH == 0 || cntG == cntH) {
+                    int len = j - i;
+                    maxLen = Math.max(maxLen, len);
+                }
             }
         }
-
-        System.out.println(max);
+                            
+        System.out.print(maxLen);
     }
 }
