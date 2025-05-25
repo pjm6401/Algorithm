09a049afb778e98990c8f0c1e93356bdd52101ec
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-26 03:35:13 +0900

    Title: 바구니 안의 사탕 2, Time: 140ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250526/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 250526/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -15,27 +15,19 @@ public class Main {
         
         int max = 0;
         
-        if(101-k<=k){
-            int sum =0;
-            for (int i = 0; i < 101;i++) sum+=basket[i];
-
-            System.out.println(sum);
-        }else{
-            for (int i = k; i < 101 -k;i++){
+        
+            for (int i = k; i < 101;i++){
              
             int sum = 0;
-            for(int j = i; j<=i+k;j++) {
+            for(int j = i-k; j<=i+k;j++) {
+                if(j>=0 && j<=100) sum += basket[j];
                 
-                sum += basket[j];
             }
 
-            for(int j = i-1; j>=i-k;j--) sum += basket[j];
 
             max = Math.max (max,sum);
             }
             System.out.println(max);
-        }
-
         
     }
 }
