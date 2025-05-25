043342e4e99a5e419653a49325a548eacb3e132b
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-25 21:25:29 +0900

    Title: 아름다운 수열 2, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250525/아름다운 수열 2/beautiful-sequence-2.java
+++ 250525/아름다운 수열 2/beautiful-sequence-2.java
@@ -1,33 +1,41 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int N = sc.nextInt();  // A 수열 길이
+        int M = sc.nextInt();  // B 수열 길이
         int[] A = new int[N];
-        for (int i = 0; i < N; i++)
-            A[i] = sc.nextInt();
         int[] B = new int[M];
-        for (int i = 0; i < M; i++)
-            B[i] = sc.nextInt();
-        
-        int cnt = 0;
-        
-        for(int i = 0; i < N - M + 1; i++){
-            int count = 0;
-            for(int j = i; j< M + i; j++){
-                for(int k = 0; k < M; k++){
-                    if(B[k] == A[j]){
-                        count++;
-                        break;
-                    }
-                }
-            }
-            if(count>=M){
-                cnt++;
-            }
+        int MAX = 1001; // 입력값 범위 가정 (0 <= 값 <= 1000)
+
+        for (int i = 0; i < N; i++) A[i] = sc.nextInt();
+        for (int i = 0; i < M; i++) B[i] = sc.nextInt();
+
+        int[] bCount = new int[MAX];
+        for (int i = 0; i < M; i++) {
+            bCount[B[i]]++;
         }
 
-        System.out.println(cnt++);
+        int cnt = 0;
+
+        for (int i = 0; i <= N - M; i++) {
+            int[] aCount = new int[MAX];
+            for (int j = i; j < i + M; j++) {
+                aCount[A[j]]++;
+            }
+
+            boolean same = true;
+            for (int k = 0; k < MAX; k++) {
+                if (aCount[k] != bCount[k]) {
+                    same = false;
+                    break;
+                }
+            }
+
+            if (same) cnt++;
+        }
+
+        System.out.println(cnt);
     }
 }
