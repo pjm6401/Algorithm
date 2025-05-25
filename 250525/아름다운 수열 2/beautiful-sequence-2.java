7a0b4a1653015b09f78b1b1d3f7d00bcf7a4e8a5
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-25 21:30:31 +0900

    Title: 아름다운 수열 2, Time: 159ms, Memory: 10MB, Status: Passed - Codetree

--- 250525/아름다운 수열 2/beautiful-sequence-2.java
+++ 250525/아름다운 수열 2/beautiful-sequence-2.java
@@ -1,39 +1,35 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();  // A 수열 길이
-        int M = sc.nextInt();  // B 수열 길이
+        int N = sc.nextInt();
+        int M = sc.nextInt();
         int[] A = new int[N];
+        for (int i = 0; i < N; i++)
+            A[i] = sc.nextInt();
         int[] B = new int[M];
-        int MAX = 1001; // 입력값 범위 가정 (0 <= 값 <= 1000)
-
-        for (int i = 0; i < N; i++) A[i] = sc.nextInt();
-        for (int i = 0; i < M; i++) B[i] = sc.nextInt();
-
-        int[] bCount = new int[MAX];
-        for (int i = 0; i < M; i++) {
-            bCount[B[i]]++;
-        }
-
+        for (int i = 0; i < M; i++)
+            B[i] = sc.nextInt();
+        
         int cnt = 0;
-
-        for (int i = 0; i <= N - M; i++) {
-            int[] aCount = new int[MAX];
-            for (int j = i; j < i + M; j++) {
-                aCount[A[j]]++;
+        Arrays.sort(B);
+        for(int i = 0; i < N - M + 1; i++){
+            int [] tmp = new int [M];
+            for(int j = 0; j< M; j++){
+                tmp[j] = A[j+i];
             }
+            Arrays.sort(tmp);
 
-            boolean same = true;
-            for (int k = 0; k < MAX; k++) {
-                if (aCount[k] != bCount[k]) {
-                    same = false;
+            boolean flag = true;
+
+            for(int j = 0; j < M; j++){
+                if(tmp[j] != B[j]){
+                    flag = false;
                     break;
                 }
             }
 
-            if (same) cnt++;
+            if(flag) cnt ++;
         }
 
         System.out.println(cnt);
