251d90b256fa57322e9e365b0335e2cf94b38d71
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-29 01:05:12 +0900

    Title: 정보에 따른 수 2, Time: 152ms, Memory: 10MB, Status: Passed - Codetree

--- 250529/정보에 따른 수 2/number-based-on-information-2.java
+++ 250529/정보에 따른 수 2/number-based-on-information-2.java
@@ -5,55 +5,35 @@ public class Main {
         int T = sc.nextInt();
         int a = sc.nextInt();
         int b = sc.nextInt();
-        //알파벳 s 를 1 , n 을 2 라고 한다
-        int [] arr = new int [1001];
+        int [] arrS = new int [1001];
+        int [] arrN = new int [1001];
+        int cntS = 0;
+        int cntN = 0;
         for (int i = 0; i < T; i++) {
             char c = sc.next().charAt(0);
             int pos = sc.nextInt();
             if(c == 'S'){
-                arr[pos] = 1;
+                arrS[cntS++] = pos;
             }else{
-                arr[pos] = 2;
+                arrN[cntN++] = pos;
             }
         }
         int cnt = 0;
 
         for(int i = a; i<=b; i++){
 
-            int slen1 =Integer.MAX_VALUE;
-            int slen2 =Integer.MAX_VALUE;
-            int nlen1 =Integer.MAX_VALUE;
-            int nlen2 =Integer.MAX_VALUE;
+            int distanceS = Integer.MAX_VALUE;
+            int distanceN = Integer.MAX_VALUE;
 
-            for(int j = i; j<1001; j++){
-                if(arr[j] == 1) {
-                    slen1 = j-i;
-                    break;
-                }
-            }
-            for(int j = i; j<1001; j++){
-                if(arr[j] == 2){
-                    nlen1 = j-i;
-                    break;
-                } 
-            }
-            for(int j = i; j>=1; j--){
-                if(arr[j] == 1) {
-                    slen2 = i-j;
-                    break;
-                }
-            }
-            for(int j = i; j>=1; j--){
-                if(arr[j] == 2) {
-                    nlen2 = i-j;
-                    break;
-                }
+            for(int j = 0; j<cntS; j++){
+                distanceS = Math.min(distanceS,Math.abs(arrS[j] - i));
             }
 
-            int sLen = Math.min(slen1,slen2);
-            int nLen = Math.min(nlen1,nlen2);
+            for(int j = 0; j<cntN; j++){
+                distanceN = Math.min(distanceN,Math.abs(arrN[j] - i));
+            }
 
-            if(sLen<=nLen){
+            if(distanceS<=distanceN){
                 cnt++;
             }
         }
