65eb5256f70b2fdaecfa69737eb91900234f0506
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-30 01:37:23 +0900

    Title: 좌표평면 위의 균형 2, Time: 155ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250530/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.java
+++ 250530/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.java
@@ -11,29 +11,27 @@ public class Main {
         }
         int min = Integer.MAX_VALUE;
 
-        for(int i = 1; i<=10; i++){
-            for(int j = 1; j<=10; j++){
-                if(i%2==1 || j%2==1) continue;
+        for(int i = 2; i<=10; i++){
+            if(i%2==1)continue;
+            for(int j = 2; j<=10; j++){
+                if(j%2==1)continue;
                 int one=0;
                 int two=0;
                 int three=0;
                 int four=0;
                 int max = 0;
-                for (int k = 0; i < n; i++) {
+
+                for (int k = 0; k < n; k++) {
                     if(x[k]>i && y[k]>j){
                         four++;
                     }else if(x[k]>i && y[k]<j){
                         two++;
                     }else if(x[k]<i && y[k]>j){
                         three++;
-                    }else{
+                    }else if(x[k]<i && y[k]<j){
                         one++;
                     }
                 }
-                System.out.println(one);
-                System.out.println(one);
-                System.out.println(one);
-                System.out.println(one);
                 max = Math.max(one,Math.max(two,Math.max(three,four)));
 
                 min = Math.min(min,max);
