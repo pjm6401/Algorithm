ce133b6a02b18e5fd384edd6e53049a5f9ce9ff4
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-30 02:22:06 +0900

    Title: 팀으로 하는 틱택토 2, Time: 103ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250530/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class Main {
+    static int [][] arr = new int [3][3];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inp1 = sc.next();
+        String inp2 = sc.next();
+        String inp3 = sc.next();
+        
+        String [] arrString = {inp1,inp2,inp3};
+
+        for(int i = 0; i<3; i++){
+            String intString = arrString[i];
+            for(int j = 0; j<3; j++){
+                
+                arr[i][j] = intString.charAt(j)-'0';
+            }
+        }
+        int ans = 0;
+
+        for(int i = 1; i<=8; i++){
+            for(int j = i+1; j<=9; j++){
+                if(i==j) continue;
+                if(isWin(i,j,arr[0][0],arr[0][1],arr[0][2])) ans++;
+                if(isWin(i,j,arr[1][0],arr[1][1],arr[1][2])) ans++;
+                if(isWin(i,j,arr[2][0],arr[2][1],arr[2][2])) ans++;
+                if(isWin(i,j,arr[0][0],arr[1][0],arr[2][0])) ans++;
+                if(isWin(i,j,arr[0][1],arr[1][1],arr[2][1])) ans++;
+                if(isWin(i,j,arr[0][2],arr[1][2],arr[2][2])) ans++;
+                if(isWin(i,j,arr[0][0],arr[1][1],arr[2][2])) ans++;
+                if(isWin(i,j,arr[0][2],arr[1][1],arr[2][0])) ans++;
+
+            }
+        }
+        System.out.println(ans);
+        
+    }
+
+    public static boolean isWin(int a, int b, int i1,int i2, int i3){
+        int aCount = 0;
+        int bCount = 0;
+        if(a == i1) aCount++; 
+        if(a == i2) aCount++; 
+        if(a == i3) aCount++; 
+        if(b == i1) bCount++; 
+        if(b == i2) bCount++; 
+        if(b == i3) bCount++;
+        
+        if((aCount==2 && bCount ==1 ) || (aCount==1 && bCount ==2 )){
+            return true;
+        }
+
+        return false; 
+    }
+}
