bf13659c9f0250f06741a1419a16d135961b58c6
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-31 01:38:45 +0900

    Title: A, B, C, D 찾기 2, Time: 161ms, Memory: 11MB, Status: Passed - Codetree

--- 250531/A, B, C, D 찾기 2/find-a-b-c-d-2.java
+++ 250531/A, B, C, D 찾기 2/find-a-b-c-d-2.java
@@ -3,33 +3,45 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[15];
-        for (int i = 0; i < 15; i++) {
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
+        int[] input = new int[15];
+        for (int i = 0; i < 15; i++)
+            input[i] = sc.nextInt();
 
-        // 오름차순 정렬시 가장 큰 값은 abcd, 두번째 큰 값은 bcd 의 합이다.
-        int abcd = arr[14];
-        int bcd = arr[13];
-        int acd = arr[12];
-        int abd = arr[11];
-        int a = abcd - bcd;
-        int cd = acd-a;
-        int b = bcd - cd;
-        int bd = abd -a;
-        int d = bd -b;
-        int c= cd -d; 
+        Arrays.sort(input); // 정렬
 
-        System.out.println(a+ " "+ b +" "+c + " "+ d);
+        // 가장 큰 수는 a + b + c + d 이다.
+        int total = input[14];
 
-        for(int i =a; i<=40;i++){
-            for(int j =a; j<=40;j++){
-                for(int k=a; k<=40;k++){
-                    if(!(i<=j && j<=k))continue;
+        // 가능한 모든 (a, b, c, d) 조합을 구해서 확인
+        for (int a = 1; a <= total; a++) {
+            for (int b = a; b <= total; b++) {
+                for (int c = b; c <= total; c++) {
+                    for (int d = c; d <= total; d++) {
+                        // 15개의 합을 만들어 봄
+                        ArrayList<Integer> list = new ArrayList<>();
 
-                    
-                    
+                        list.add(a); list.add(b); list.add(c); list.add(d);
+                        list.add(a + b); list.add(b + c); list.add(c + d); list.add(d + a);
+                        list.add(a + c); list.add(b + d);
+                        list.add(a + b + c); list.add(a + b + d);
+                        list.add(a + c + d); list.add(b + c + d);
+                        list.add(a + b + c + d);
+
+                        Collections.sort(list);
+
+                        boolean match = true;
+                        for (int i = 0; i < 15; i++) {
+                            if (list.get(i) != input[i]) {
+                                match = false;
+                                break;
+                            }
+                        }
+
+                        if (match) {
+                            System.out.println(a + " " + b + " " + c + " " + d);
+                            return;
+                        }
+                    }
                 }
             }
         }
