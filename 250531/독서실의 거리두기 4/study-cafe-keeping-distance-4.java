cf64469de11bbdef8fd81cb1288754cdd47a5562
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-05-31 02:21:38 +0900

    Title: 독서실의 거리두기 4, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250531/독서실의 거리두기 4/study-cafe-keeping-distance-4.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String seat = sc.next();
+        
+        int [] desk = new int [n];
+
+        for(int i = 0; i<n; i++){
+            if(seat.charAt(i) == '1') desk[i] = 1;
+        }
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            if(desk[i] == 1) continue;
+            for(int j = i+1; j < n; j++){
+                if(desk[j] == 1) continue;
+                desk[i] = 1;
+                desk[j] = 1;
+
+                int min = minimum(desk);
+
+                max = Math.max(max,min);
+                desk[i] = 0;
+                desk[j] = 0;
+            }
+        }
+        System.out.println(max);
+    }
+
+
+    public static int minimum(int [] arr){
+        int index = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] ==1 ){
+                int length = i-index;
+                if(length != 0){
+                    min = Math.min(min,length);
+                }
+                
+                index = i;
+            }
+        }
+        return min;
+    }
+}
