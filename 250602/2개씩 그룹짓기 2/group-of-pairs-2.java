f6ed64dfbc1de295e9587152f089b4fa24975237
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-06-02 14:37:51 +0900

    Title: 2개씩 그룹짓기 2, Time: 440ms, Memory: 24MB, Status: Wrong Answer - Codetree

--- 250602/2개씩 그룹짓기 2/group-of-pairs-2.java
+++ 250602/2개씩 그룹짓기 2/group-of-pairs-2.java
@@ -5,27 +5,6 @@ public class Main {
     static int n;
     static int[] nums;
 
-    public static boolean isPossible(int minDiff) {
-        // 그룹 수
-        int count = 0;
-        boolean[] used = new boolean[2 * n];
-
-        for (int i = 0; i < 2 * n; i++) {
-            if (used[i]) continue;
-
-            for (int j = i + 1; j < 2 * n; j++) {
-                if (used[j]) continue;
-                if (Math.abs(nums[i] - nums[j]) >= minDiff) {
-                    used[i] = true;
-                    used[j] = true;
-                    count++;
-                    break;
-                }
-            }
-        }
-        return count >= n;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -33,23 +12,14 @@ public class Main {
         for (int i = 0; i < 2 * n; i++) {
             nums[i] = sc.nextInt();
         }
-
+        int max = 0;
         Arrays.sort(nums);
 
-        int left = 0;
-        int right = nums[2 * n - 1] - nums[0];
-        int answer = 0;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (isPossible(mid)) {
-                answer = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+        for(int i = 0; i<2*n; i+=2){
+            max = Math.max(max, nums[i+1] - nums[i]);
         }
 
-        System.out.println(answer);
+        System.out.println(max);
     }
 }
