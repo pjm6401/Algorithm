75ed67fbb7d9fa9b8da6bbc4031b1c071e650d1d
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-06-03 16:56:57 +0900

    Title: 코딩톡, Time: 137ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250603/코딩톡/coding-talk.java
+++ 250603/코딩톡/coding-talk.java
@@ -14,11 +14,18 @@ public class Main {
         }
         //p 번째 메세지를 보낸사람과, 이후 채팅방에 메세지를 남긴 사람은 모두 읽음.
         for (int i = p-1; i < M; i++) {
-            int charInt = c[i] - 'A';
-            check[charInt] = true;
+            if(u[p-1] == 0){
+                
+                check[i] = true;
+            }else{
+                int charInt = c[i] - 'A';
+                check[charInt] = true;
+            }
         }
 
         for(int i =0; i<N; i++){
+            //System.out.print(check[i]+" ");
+            
             if(!check[i]){
                 char ch = (char)(i+65);
                 System.out.print(ch+" ");
