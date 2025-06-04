2435f473b10b6d3d6963974dee530b48d25d20dc
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-06-05 01:35:11 +0900

    Title: 순위 경쟁, Time: 148ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250605/순위 경쟁/ranking-competition.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int start = 6;
+        int scoreA = 0;
+        int scoreB = 0;
+        int scoreC = 0;
+        int winA = 1;
+        int winB = 2;
+        int winC = 3;
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            char c = sc.next().charAt(0);
+            int s = sc.nextInt();
+            int now = 0;
+            if(c == 'A'){
+                scoreA += s;
+            }else if (c == 'B'){
+                scoreB += s;
+            }else{
+                scoreC += s;
+            }
+            
+            int max = Math.max(scoreA,Math.max(scoreB,scoreC));
+
+            if(scoreA == max){
+                now += winA;
+            }
+            if(scoreB == max){
+                now += winB;
+            }
+            if(scoreC == max){
+                now += winC;
+            }
+            
+
+            if(start!=now){
+                cnt++;
+            }
+            start = now;
+        }
+        System.out.print(cnt);
+        // Please write your code here.
+    }
+}
