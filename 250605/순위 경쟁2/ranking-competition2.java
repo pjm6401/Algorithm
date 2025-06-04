73342aec93b738179d3614d1f5274cf0b1d3c13e
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-06-05 00:18:06 +0900

    Title: 순위 경쟁2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250605/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int scoreA =0;
+        int scoreB =0;
+        int start = -1;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            char c = sc.next().charAt(0);
+            int s = sc.nextInt();
+            if(c =='A'){
+                scoreA += s;
+            }else{
+                scoreB +=s;
+            }
+        
+            if(scoreA > scoreB){
+                int now = 0;
+                if(start != now){
+                cnt ++;
+                start = now;
+            }
+            }else if(scoreA < scoreB){
+                int now = 1;
+                if(start != now){
+                cnt ++;
+                start = now;
+            }
+            }else {
+                int now = 2;
+                if(start != now){
+                cnt ++;
+                start = now;
+            }
+            }
+
+        }
+        System.out.print(cnt);
+        // Please write your code here.
+    }
+}
