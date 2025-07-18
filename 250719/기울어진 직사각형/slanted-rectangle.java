a39cb31f247198025b6259f65787afa613b7192f
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-07-19 01:53:55 +0900

    Title: 기울어진 직사각형, Time: 158ms, Memory: 11MB, Status: Passed - Codetree

--- 250719/기울어진 직사각형/slanted-rectangle.java
+++ 250719/기울어진 직사각형/slanted-rectangle.java
@@ -1,81 +1,73 @@
 import java.util.Scanner;
-public class Main {
 
+public class Main {
     static int n;
-    static int [] dx = {-1,-1,0,1,1,1,0,-1};
-    static int [] dy = {0,1,1,1,0,-1,-1,-1};
+    static int[][] grid;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int[][] grid = new int[n][n];
-        boolean [][] visit = new boolean[n][n];
+        grid = new int[n][n];
+
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
+
         int max = 0;
-        for(int i = 1; i < n-1; i++){
-            for(int j = 1; j < n-1; j++){
-                max = Math.max(calc(grid,i,j,visit),max);
+        // (i,j) = 시작 꼭짓점
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // 가능한 모든 길이 시도
+                for (int len1 = 1; i + len1 < n && j - len1 >= 0; len1++) {
+                    for (int len2 = 1; i + len1 + len2 < n && j + len2 < n; len2++) {
+                        if (j - len1 + len2 < n && j - len1 + len2 >= 0) {
+                            max = Math.max(max, getPathSum(i, j, len1, len2));
+                        }
+                    }
+                }
             }
         }
+
         System.out.println(max);
     }
 
-    public static int calc(int [][] grid, int i , int j, boolean [][] visit){
-        if (visit[i][j]) return 0;
-        int dxA = i+dx[0];
-        int dyA = j+dy[0];
-        int dxB = i+dx[2];
-        int dyB = j+dy[2];
-        int dxC = i+dx[4];
-        int dyC = j+dy[4];
-        int dxD = i+dx[6];
-        int dyD = j+dy[6];
-        int sumA = grid[dxA][dyA] + grid[dxB][dyB] + grid[dxC][dyC] + grid[dxD][dyD];
-        int sumB = grid[dxA][dyA] + grid[dxB][dyB] + grid[dxC][dyC] + grid[dxD][dyD];
-        visit[i][j] = true;
-        
-        boolean flag1 = true;
-        boolean flag2 = true;
-        while(flag1 || flag2){
-            if(dxA+dx[1]>=0 && dyB+dy[1]<n){
-                dxA+=dx[1];
-                dyA+=dy[1];
-                dxB+=dx[1];
-                dyB+=dy[1];
-                sumA += grid[dxA][dyA] + grid[dxB][dyB];
-                visit[dxB][dyB-1] = true;
-            }else if(dxC+dx[5]<n && dyD+dy[5]>=0){
-                dxC+=dx[5];
-                dyC+=dy[5];
-                dxD+=dx[5];
-                dyD+=dy[5];
-                sumA += grid[dxC][dyC] + grid[dxD][dyD];
-                visit[dxD][dyD+1] = true;
-            }else{
-                flag1 = false;
-            }
+    // i,j: 시작점, len1: 왼쪽 아래 길이, len2: 오른쪽 아래 길이
+    public static int getPathSum(int i, int j, int len1, int len2) {
+        int sum = 0;
+        int x = i, y = j;
 
-            if(dxA+dx[7]>=0 && dyD+dy[7]>=0){
-                dxA+=dx[7];
-                dyA+=dy[7];
-                dxD+=dx[7];
-                dyD+=dy[7];
-                sumB += grid[dxA][dyA] + grid[dxD][dyD];
-                visit[dxD][dyD+1] = true;
-            }else if(dyB+dy[3]<n && dxC+dx[3]>=0){
-                dxB+=dx[3];
-                dyB+=dy[3];
-                dxC+=dx[3];
-                dyC+=dy[3];
-               
-                sumB += grid[dxB][dyB] + grid[dxC][dyC];
-                visit[dxB][dyB-1] = true;
-            }else{
-                flag2 = false;
-            }
+        // 왼쪽 아래
+        for (int k = 0; k < len1; k++) {
+            x++; y--;
+            if (!isInBounds(x, y)) return 0;
+            sum += grid[x][y];
         }
-        return Math.max(sumA,sumB);
+
+        // 오른쪽 아래
+        for (int k = 0; k < len2; k++) {
+            x++; y++;
+            if (!isInBounds(x, y)) return 0;
+            sum += grid[x][y];
+        }
+
+        // 오른쪽 위
+        for (int k = 0; k < len1; k++) {
+            x--; y++;
+            if (!isInBounds(x, y)) return 0;
+            sum += grid[x][y];
+        }
+
+        // 왼쪽 위
+        for (int k = 0; k < len2; k++) {
+            x--; y--;
+            if (!isInBounds(x, y)) return 0;
+            sum += grid[x][y];
+        }
+
+        return sum;
     }
-    
+
+    public static boolean isInBounds(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < n;
+    }
 }
