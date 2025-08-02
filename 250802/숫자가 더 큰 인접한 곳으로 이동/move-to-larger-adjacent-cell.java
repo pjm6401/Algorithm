8b6797fb9c6d142e5909a991f2913acf1fbb4c25
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-02 23:06:51 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 111ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250802/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Main {
+    static int n;
+    static int r;
+    static int c;
+    static int [][] grid = new int[101][101];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        r = sc.nextInt();
+        c = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        shift(grid);
+    }
+
+    public static void shift(int [][] grid){
+        int [] dx = {0,1,0,-1};
+        int [] dy = {1,0,-1,0};
+        int x = r-1;
+        int y = c-1;
+        int tempX = x;
+        int tempY = y;
+        boolean flag = true;
+        while(flag){
+            flag = false;
+            for(int i = 0; i<4; i++){
+                if(isRange(x+dx[i],y+dy[i]) && grid[x][y]< grid[x+dx[i]][y+dy[i]]){
+                    tempX = x+dx[i];
+                    tempY = y+dy[i];
+                    flag = true;
+                }
+            }
+            if(flag){
+                x = tempX;
+                y = tempY;
+                System.out.print(grid[tempX][tempY]+" ");
+            }
+        }
+        
+    }
+
+    public static boolean isRange(int x, int y){
+        return (x>=0 && x<n && y>=0 && y<n);
+    }
+}
