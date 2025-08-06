c68e00c35402e373d7548cb99fc080924983b80d
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-06 23:58:12 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250806/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
+++ 250806/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
@@ -5,6 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
         int n = sc.nextInt();
+
+        if(n==1){
+            for(int i = 1; i<=k; i++){
+                System.out.println(i);
+            }
+            return;
+        }
+
         for(int i = 1; i<=k; i++){
             for(int j = 1; j<=k; j++){
                 System.out.println(i+" "+j);
