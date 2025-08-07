0c2dd989caa55b7b78e18d030ca6a711239a0a28
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-07 23:24:07 +0900

    Title: 겹치지 않게 선분 고르기, Time: 140ms, Memory: 11MB, Status: Wrong Answer - Codetree

--- 250807/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 250807/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -2,38 +2,77 @@ import java.util.*;
 
 public class Main {
 
-    static class Line {
-        int start, end;
-        public Line(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
-
+    static ArrayList<Line> lines = new ArrayList<>();
+    static ArrayList<Line> anslines = new ArrayList<>();
+    static int lineSize = 0;
+    static int lineSizeTemp = 0;
+    
+    static int max = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        List<Line> lines = new ArrayList<>();
-
+        int[][] segments = new int[n][2];
         for (int i = 0; i < n; i++) {
             int start = sc.nextInt();
             int end = sc.nextInt();
-            lines.add(new Line(start, end));
+            Line line = new Line(start,end);
+            lines.add(line);
         }
 
-        // 끝나는 시간 기준으로 정렬
-        lines.sort(Comparator.comparingInt(a -> a.end));
+        lineSize = lines.size();
+        lineSizeTemp = lineSize;
 
-        int count = 0;
-        int lastEnd = Integer.MIN_VALUE;
+        calc(0);
 
-        for (Line line : lines) {
-            if (line.start >= lastEnd) {
-                count++;
-                lastEnd = line.end;
+        System.out.println(max);
+    }
+
+    public static void calc(int cnt){
+        if(cnt == lineSize){
+            int ans = countLine();
+            max = Math.max(max,ans);
+            return;
+        }
+        int tempCount = countLine() + lineSize - cnt;
+        if(max>=tempCount) return;
+        for(int i = cnt; i<lineSize; i++){
+            Line line = lines.get(cnt);
+            anslines.add(line);
+            calc(cnt + 1);
+            anslines.remove(anslines.size()-1);
+        }
+    }
+    public static int countLine(){
+        int max = 0;
+        for(int i = 0; i<anslines.size(); i++){
+            Line line = lines.get(i);
+            int ans = 1;
+            for(int j = 0; j<anslines.size(); j++){
+                if(i!=j){
+                    Line lineTemp = lines.get(j);
+                    if(lineTemp.getEnd()<line.getStart() || lineTemp.getStart() > line.getEnd()){
+                        ans++;
+                    }
+                }
             }
+            max = Math.max(max,ans);
         }
+        return max;
+    }
+}
 
-        System.out.println(count);
+class Line {
+    int start;
+    int end;
+    public Line(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart(){
+        return start;
+    }
+    public int getEnd(){
+        return end;
     }
 }
