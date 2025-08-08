969f0f12746fee3f180a97d2324798b1d3f37cac
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-09 01:06:43 +0900

    Title: 사각형 채우기, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

--- 250809/사각형 채우기/rectangle-fill.java
+++ 250809/사각형 채우기/rectangle-fill.java
@@ -1,26 +1,28 @@
 import java.util.Scanner;
+
 public class Main {
-
-
+    public static final int MOD = 10007;
     public static final int MAX_N = 1000;
+    
+    // 변수 선언
+    public static int n;
+    
     public static int[] dp = new int[MAX_N + 1];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        // 입력:
+        n = sc.nextInt();
 
-        // 초기값 설정
+        // 초기 조건 설정
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
 
-        // 점화식을 이용하여 dp 배열 채우기
+        // 점화식에 따라 dp값 채우기
         // dp[i] = dp[i - 1] + dp[i - 2]
-        for (int i = 3; i <= n; i++) {
-            // 문제에 따라 결과값을 특정 수로 나눈 나머지를 요구할 수 있습니다.
-            // 예: dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-            dp[i] = dp[i - 1] + dp[i - 2]  % 10007 ;
-        }
+        for(int i = 2; i <= n; i++)
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
 
-        System.out.println(dp[n]);
+        System.out.print(dp[n]);
     }
 }
