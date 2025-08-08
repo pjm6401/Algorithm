c8ecc63f0b9b96ec221ebaaa4937d583ed33f072
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-09 00:07:54 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250809/피보나치 수/fibonacci-number.java
+++ 250809/피보나치 수/fibonacci-number.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 public class Main {
+
+    static int [] memo = new int [46];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -9,10 +12,15 @@ public class Main {
     }
 
     public static int fibo(int n){
+        if(memo[m] != 0){
+            return memo[n];
+        }
         if(n == 1 || n==2){
-            return 1;
+            memo[n] = 1;
+        }else{
+            memo[n] = memo[n-1] + memo[n-2];
         }
 
-        return fibo(n-1) + fibo(n-2);
+        return memo[n];
     }
 }
