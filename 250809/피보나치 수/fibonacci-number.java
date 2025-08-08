f72bfaa0ca6a23c2388d47d4556b17c03f3e6ff5
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-09 00:14:18 +0900

    Title: 피보나치 수, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250809/피보나치 수/fibonacci-number.java
+++ 250809/피보나치 수/fibonacci-number.java
@@ -6,21 +6,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int result = fibo(n);
-
-        System.out.println(result);
+        for(int i = 0; i<46; i++){
+            memo[i] = -1;
+        }
+        System.out.println(fibbo(n));
     }
 
-    public static int fibo(int n){
-        if(memo[m] != 0){
-            return memo[n];
-        }
-        if(n == 1 || n==2){
-            memo[n] = 1;
-        }else{
-            memo[n] = memo[n-1] + memo[n-2];
+    public static int fibbo(int n) {
+        // 이미 n번째 값을 구해본 적이 있다면 (memo[n]이 -1이 아니라면)
+        if (memo[n] != -1) {
+            return memo[n]; // memo에 저장된 값을 즉시 반환
         }
 
+        // 종료 조건: n이 2 이하인 경우
+        if (n <= 2) {
+            memo[n] = 1; // F(1)과 F(2)는 1로 정의
+        }
+        // 재귀 호출: n이 3 이상인 경우
+        else {
+            // 점화식을 이용해 답을 구하고, 그 결과를 memo에 저장
+            memo[n] = fibbo(n - 1) + fibbo(n - 2);
+        }
+
+        // memo에 저장된 n번째 값을 반환
         return memo[n];
     }
 }
