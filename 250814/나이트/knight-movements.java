045e43e124bd9b9d24e4c68a9d0d1bc2e70a0560
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-14 20:01:21 +0900

    Title: 나이트, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250814/나이트/knight-movements.java
+++ 250814/나이트/knight-movements.java
@@ -1,72 +1,45 @@
 import java.util.*;
+
 public class Main {
-
-    static int n;
-    static int r1;
-    static int c1;
-    static int r2;
-    static int c2;
-
-    static int [] dx = {-2,-1,1,2,2,1,-1,-2};
-    static int [] dy = {1,2,2,1,-1,-2,-2,-1};
-    static Deque<Index> dq = new ArrayDeque<>();
-    static boolean [][] visit = new boolean[100][100];
-    static int [][] step = new int [100][100];
-    static int min = 10000;
-    static int ans = min;
+    static int n, r1, c1, r2, c2;
+    static int[][] step = new int[100][100];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        r1 = sc.nextInt()-1;
-        c1 = sc.nextInt()-1;
-        r2 = sc.nextInt()-1;
-        c2 = sc.nextInt()-1;
-        dq.add(new Index(r1,c1));
-        step[r1][c1] = 0;
-        BFS();
-        if(ans == min){
-            ans = -1;
-        }
+        r1 = sc.nextInt() - 1;
+        c1 = sc.nextInt() - 1;
+        r2 = sc.nextInt() - 1;
+        c2 = sc.nextInt() - 1;
 
-        System.out.println(ans);
+        for (int i = 0; i < n; i++) Arrays.fill(step[i], -1);
+
+        System.out.println(BFS());
     }
 
-    public static void BFS(){
-        while(!dq.isEmpty()){
-            Index index = dq.poll();
+    public static int BFS() {
+        int[] dx = {-2,-1,1,2,2,1,-1,-2};
+        int[] dy = {1,2,2,1,-1,-2,-2,-1};
 
-            int x = index.x;
-            int y = index.y;
-            visit[x][y] = true;
+        Queue<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[]{r1, c1});
+        step[r1][c1] = 0;
 
-            if(x==r2 && y==c2){
-                ans = Math.min(ans,step[x][y]);
-                break;
-            }
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
+            int x = cur[0], y = cur[1];
 
-            for(int i = 0; i<8; i++){
+            if (x == r2 && y == c2) return step[x][y];
+
+            for (int i = 0; i < 8; i++) {
                 int nx = x + dx[i];
-                int ny = y + dy[i]; 
-
-                if(isRange(nx,ny)){
-                    dq.add(new Index(nx,ny));
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && step[nx][ny] == -1) {
                     step[nx][ny] = step[x][y] + 1;
+                    dq.add(new int[]{nx, ny});
                 }
             }
-            
         }
-    }
-
-    public static boolean isRange(int x, int y){
-        return (x>=0 && x<n && y>=0 && y<n && !visit[x][y]);
-    }
-}
-class Index {
-    int x;
-    int y;
-    public Index(int x, int y){
-        this.x = x;
-        this.y = y;
+        return -1;
     }
 }
