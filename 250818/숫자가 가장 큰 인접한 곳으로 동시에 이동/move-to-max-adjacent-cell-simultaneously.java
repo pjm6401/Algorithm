12cbdbe40412c2b4ab6f9e43987ce9efbcff6a13
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-18 00:20:31 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 153ms, Memory: 10MB, Status: Passed - Codetree

--- 250818/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
+++ 250818/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -1,94 +1,85 @@
 import java.util.*;
+
 public class Main {
 
     static int n;
-    static int m;
     static int t;
-    static int[][] grid = new int[20][20];
-    static boolean [][] visit = new boolean [20][20];
-    static Deque<int []> dq = new ArrayDeque<>();
+    static int[][] grid;
+    // 방향 벡터: 상, 하, 좌, 우
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        m = sc.nextInt();
+        int m = sc.nextInt(); // 초기 구슬 개수, 전역 변수로 필요 없음
         t = sc.nextInt();
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
+
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 grid[i][j] = sc.nextInt();
-        
-        int [][] temp = new int [n][n];
-
-        for (int i = 0; i < m; i++) {
-            int x = sc.nextInt()-1;
-            int y = sc.nextInt()-1;
-            visit[x][y] = true;
-            dq.add(new int [] {x,y});
-            temp[x][y] = 1;
+            }
         }
-        int [] dx = {0,0,1,-1};
-        int [] dy = {-1,1,0,0};
-        for(int i = 0; i<t; i++){
-            for(int k = 0; k < dq.size(); k++){
-                int [] arr = dq.poll();
-                int x = arr[0];
-                int y = arr[1];
-                if(!visit[x][y]) continue;
-                int tx = x;
-                int ty = y;
-                int now = grid[x][y];
-                int max = grid[x][y];
 
-                for(int j = 0; j<4; j++){
-                    int nx = x+dx[j];
-                    int ny = y+dy[j];
+        // 현재 구슬들의 위치를 저장하는 큐
+        Queue<int[]> ballQueue = new ArrayDeque<>();
+        for (int i = 0; i < m; i++) {
+            int r = sc.nextInt() - 1;
+            int c = sc.nextInt() - 1;
+            ballQueue.add(new int[]{r, c});
+        }
 
-                    if(isRange(nx,ny) && grid[nx][ny] >= max){
-                        max = grid[nx][ny];
-                        tx = nx;
-                        ty = ny;
+        // t초 동안 시뮬레이션 진행
+        for (int time = 0; time < t; time++) {
+            // 이번 시간 단계에서 구슬이 이동할 위치와 해당 위치에 도달할 구슬의 수를 기록
+            // key: 위치(r*n+c), value: 해당 위치로 이동할 구슬의 수
+            Map<Integer, int[]> nextPositions = new HashMap<>();
+
+            // 현재 큐에 있는 모든 구슬(이번 턴에 움직일 구슬)들을 처리
+            while (!ballQueue.isEmpty()) {
+                int[] currentPos = ballQueue.poll();
+                int r = currentPos[0];
+                int c = currentPos[1];
+
+                int maxVal = -1;
+                int nextR = r;
+                int nextC = c;
+
+                // 4방향 탐색하여 우선순위에 따라 다음 위치 결정
+                for (int i = 0; i < 4; i++) {
+                    int nr = r + dx[i];
+                    int nc = c + dy[i];
+
+                    if (isRange(nr, nc)) {
+                        if (grid[nr][nc] > maxVal) {
+                            maxVal = grid[nr][nc];
+                            nextR = nr;
+                            nextC = nc;
+                        }
                     }
                 }
-                if(now != max){
-                    temp[tx][ty] +=1;
-                    temp[x][y] -=1;
-                    visit[tx][ty] = true;
-                    visit[x][y] = false;
-                    dq.add(new int[]{tx,ty});
-                }
-            }
-            temp = newGrid(temp);
-        }
-
-        System.out.println(clacBall(temp));
-    }
-    public static boolean isRange(int x, int y){
-        return( x>=0 && x<n && y>=0 && y<n);
-    }
-
-    public static int [][] newGrid(int [][] grid){
-        int [][] temp = new int [n][n];
-        for(int i = 0; i < n ; i++){
-            for(int j = 0; j < n; j++){
-                temp[i][j] = grid[i][j];
-                if(temp[i][j] >=2) {
-                    temp[i][j] = 0;
-                    visit[i][j] = false;
-                }
-            }
-        }
-
-        return temp;
-    }
-
-    public static int clacBall(int [][]grid){
-        int cnt = 0;
-        for(int i = 0; i < n ; i++){
-            for(int j = 0; j < n; j++){
                 
-                if(grid[i][j] ==1) cnt++;
+                // 다음 위치를 Map에 기록
+                int posKey = nextR * n + nextC;
+                nextPositions.putIfAbsent(posKey, new int[]{nextR, nextC, 0});
+                nextPositions.get(posKey)[2]++; // count 증가
+            }
+
+            // 충돌 처리 후 살아남은 구슬들을 다음 턴을 위해 큐에 다시 추가
+            for (int[] posInfo : nextPositions.values()) {
+                // 해당 위치로 이동한 구슬이 1개인 경우에만 살아남음
+                if (posInfo[2] == 1) {
+                    ballQueue.add(new int[]{posInfo[0], posInfo[1]});
+                }
             }
         }
-        return cnt;
+
+        // t초 후 남아있는 구슬의 개수 출력
+        System.out.println(ballQueue.size());
+    }
+
+    public static boolean isRange(int r, int c) {
+        return r >= 0 && r < n && c >= 0 && c < n;
     }
 }
