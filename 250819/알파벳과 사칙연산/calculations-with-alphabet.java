6a051ee12eccb06e1e45fa78dbe670a2f62d0ba7
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-19 18:48:53 +0900

    Title: 알파벳과 사칙연산, Time: 164ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 250819/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class Main {
+    static List<Integer> list = new ArrayList<>();
+    static int ans = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String expression = sc.next();
+        char[] arr = expression.toCharArray();
+        char[] arrChar = new char[(arr.length/2)+1];
+        int index = 0;
+        for(int i = 0; i<arr.length; i+=2){
+            arrChar[index] = arr[i];
+            index++;
+        }
+        BackTracking(0,arr,arrChar);
+        System.out.println(ans);
+    }
+
+    static void BackTracking(int cnt,char[] arr,char [] arrChar){
+        if(cnt == arrChar.length){
+            int result = 0;
+            Map<Character,Integer> map = new HashMap<>();
+            for(int i = 0; i<arrChar.length; i++){
+                int value = list.get(i);
+                map.put(arrChar[i],value);
+            }
+
+            for(int i = 0; i<arr.length; i+=2){
+                if(i==0){
+                    result = map.get(arr[i]);
+                    continue;
+                }
+                if(arr[i-1] == '+'){
+                    result += map.get(arr[i]);
+                }else if(arr[i-1] == '-'){
+                    result -= map.get(arr[i]);
+                }else if(arr[i-1] == '*'){
+                    result *= map.get(arr[i]);
+                }
+            }
+            
+            ans = Math.max(ans,result);
+
+            return;
+        }
+
+        for(int i = 1; i<=4; i++){
+            list.add(i);
+            BackTracking(cnt+1, arr, arrChar);
+            list.remove(list.size()-1);
+        }
+    }
+}
