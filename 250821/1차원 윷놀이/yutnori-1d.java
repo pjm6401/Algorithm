644e8f68982da374183ea0864530a9260e6e696a
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-21 00:37:38 +0900

    Title: 1차원 윷놀이, Time: 2138ms, Memory: 12MB, Status: Passed - Codetree

--- 250821/1차원 윷놀이/yutnori-1d.java
+++ 250821/1차원 윷놀이/yutnori-1d.java
@@ -6,6 +6,7 @@ public class Main {
     static int k;
     static List<Integer> list = new ArrayList<>();
     static int max = 0;
+    static int [] mal = new int[101];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -15,6 +16,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
+        Arrays.fill(mal,1);
         backTraking(0,nums);
 
         System.out.println(max);
@@ -23,16 +25,14 @@ public class Main {
     public static void backTraking(int cnt,int [] nums){
         if(cnt == n){
             int ans = 0;
-            int [] mal = newArr();
-            for(int i = 0; i<n; i++){
+            for(int i=0;i<n;i++){
                 mal[list.get(i)] += nums[i];
             }
-
-            for(int i = 1; i<=m; i++){
+            for(int i=1;i<=m;i++){
                 if(mal[i]>=m) ans++;
             }
-
-            max = Math.max(ans,max);
+            max = Math.max(max, ans);
+            Arrays.fill(mal,1);
             return;
         }
 
