6fc64e9dece15fe7a3bfb523803ad6ab67b5a240
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-23 02:41:01 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250823/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class Main {
+
+    static List<Integer> list = new ArrayList<>();
+    static List<Integer> listAnother = new ArrayList<>();
+
+    static int [] arr = new int [20];
+    static int n ;
+    static int sum = 0;
+    static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = 0; i < 2 * n; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        
+        choice(0,0);
+
+        System.out.println(min);
+    }
+
+    public static void choice(int cnt,int index){
+        if(cnt == n){
+            int a = 0;
+
+            for(int i = 0; i<n; i++){
+                a+=arr[list.get(i)];
+            }
+
+            int b = sum - a;
+            
+            int result = Math.abs(a-b);
+
+            min = Math.min(min,result);
+
+            return;
+        }
+
+        for(int i = index; i<2*n; i++){
+            list.add(i);
+            choice(cnt+1, index+1);
+            list.remove(list.size()-1);
+        }
+    }
+
+    public static void choiceAnother(int cnt){
+
+    }
+}
