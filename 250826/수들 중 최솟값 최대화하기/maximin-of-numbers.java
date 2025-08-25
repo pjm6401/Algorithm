41a91c791988967535e01a31e25b11423223f077
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-26 01:25:46 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250826/수들 중 최솟값 최대화하기/maximin-of-numbers.java
+++ 250826/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -7,6 +7,7 @@ public class Main {
     public static boolean[] visit;
     public static List<int []> list = new ArrayList<>();
     public static int max = 0;
+    public static int min = 10001;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -17,27 +18,22 @@ public class Main {
                 grid[i][j] = sc.nextInt();
             }
         }
-        choice(0);
+        choice(0,10001);
         System.out.println(max);
     }
 
-    public static void choice(int cnt){
+    public static void choice(int cnt,int answer){
         if(cnt == n){
-            int min = 10001;
-            for(int [] arr : list){
-                min = Math.min(min,grid[arr[0]][arr[1]]);
-            }
-
             max = Math.max(min,max);
             return;
         }
-
+        min = Math.min(min,answer);
         for(int i = 0; i<n; i++){
             if(!visit[i]){
                 visit[i] = true;
-                list.add(new int [] {cnt, i});
-                choice(cnt+1);
-                list.remove(list.size()-1);
+                //list.add(new int [] {cnt, i});
+                choice(cnt+1,grid[cnt][i]);
+                //list.remove(list.size()-1);
                 visit[i] = false;
             }
         }
