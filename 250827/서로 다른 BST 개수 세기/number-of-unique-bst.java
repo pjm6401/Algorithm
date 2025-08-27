87e0aee24c57fcf429dd366af91fd236a42d2dde
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-08-27 21:40:43 +0900

    Title: 서로 다른 BST 개수 세기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250827/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 250827/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
+
 public class Main {
+    public static final int MAX_NUM = 19;
+    
+    public static int[] dp = new int[MAX_NUM + 1];
+    
+    public static int numOfUniqueBST(int n) {
+        int numberOfUniqueBst = 0;
+    
+        for(int i = 0; i < n; i++)
+            numberOfUniqueBst += dp[i] * dp[n - i - 1];
+    
+        return numberOfUniqueBst;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int [] arr = new int [20];
 
-        arr[0] = 0;
-        arr[1] = 1;
-        arr[2] = 2;
-        arr[3] = 5;
-        for(int i =4; i<=n; i++){
-            arr[i] = 2*arr[i-1] + 2*arr[i-2];
-        }
+        dp[0] = 1;
+        dp[1] = 1;
 
-        System.out.println(arr[n]);
+        for(int i = 2; i <= n; i++)
+            dp[i] = numOfUniqueBST(i);
+
+        System.out.print(dp[n]);
     }
 }
