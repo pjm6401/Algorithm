4f4e42497e7a0fcc76549228767395d4455f71a7
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-03 01:33:17 +0900

    Title: 최대 점프 횟수, Time: 180ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250903/최대 점프 횟수/maximum-number-of-jumps.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int [] dp = new int [n];
+        dp[0] = 0;
+        for(int i = 1; i<n; i++){
+            dp[i] = -1;
+        }
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<i; j++){
+                if(dp[j] == -1) continue;
+                if(j+arr[j]>=i){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+
+        int max = 0;
+
+        for(int i : dp){
+            max = Math.max(i,max);
+        }
+
+        System.out.println(max);
+    }
+}
