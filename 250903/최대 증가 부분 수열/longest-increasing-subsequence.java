1cccb025af888019c324546501d8b3d8e34f2769
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-03 01:24:14 +0900

    Title: 최대 증가 부분 수열, Time: 231ms, Memory: 13MB, Status: Passed - Codetree

--- 250903/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 250903/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -7,7 +7,7 @@ public class Main {
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
         int [] dp = new int[n];
-        dp[0]=1;
+        for(int i=0; i<n; i++) dp[i] = 1;
 
         for(int i =1; i<n; i++){
             for(int j = 0; j<i; j++){
