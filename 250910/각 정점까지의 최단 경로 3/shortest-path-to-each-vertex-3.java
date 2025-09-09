a8cd31da3f292aab5bf7402bae9160aad8dbde7c
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-10 03:22:11 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 128ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250910/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int [][] graph = new int[m+1][m+1];
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            graph[u][v] = w;
+        }
+        int [] dist = new int [n+1];
+
+        for(int i = 1; i<=n; i++){
+            dist[i] = 10001;
+        }
+        dist[1] = 0;
+        boolean [] visit = new boolean [n+1];
+        for(int i = 1; i<=n; i++){
+            int minIdx = -1;
+            for(int j = 1; j<=n; j++){
+                if(visit[j]) continue;
+
+                if(minIdx ==-1 || dist[minIdx]>dist[j]){
+                    minIdx = j;
+                }
+            }
+            
+            visit[minIdx] = true;
+
+            for(int j=1; j<=n; j++){
+                if(graph[minIdx][j] == 0)
+                    continue;
+
+                dist[j] = Math.min(dist[j],dist[minIdx]+graph[minIdx][j]);
+            }
+        }
+        for(int i = 2; i<=n; i++){
+            System.out.println(dist[i]);
+        }
+    }
+}
