391d40a0990b9c9903275246792be074755a24f7
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-10 01:19:44 +0900

    Title: 편집거리 3, Time: 161ms, Memory: 14MB, Status: Passed - Codetree

--- 250910/편집거리 3/edit-distance-3.java
+++ 250910/편집거리 3/edit-distance-3.java
@@ -6,26 +6,19 @@ public class Main {
         String b = sc.next();
         int n = a.length();
         int m = b.length();
+        int [][] dp = new int [n+1][m+1];
 
-        a = "!"+a;
-        b = "!"+b;
-
-       int[][] dp = new int[n + 1][m + 1];
-
-        // LCS 계산
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (a.charAt(i - 1) == b.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        for (int i = 1; i <= n; i++) dp[i][0] = i;
+        for (int j = 1; j <= m; j++) dp[0][j] = j;
+        for(int i = 1; i<=n;i++){
+            for(int j = 1; j<=m; j++){
+                if(a.charAt(i-1) == b.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{  
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
                 }
             }
         }
-
-        int lcsLength = dp[n][m];
-        int editDistance = n + m - 2 * lcsLength; // 삽입/삭제만 고려
-
-        System.out.println(editDistance);
+        System.out.print(dp[n][m]);
     }
 }
