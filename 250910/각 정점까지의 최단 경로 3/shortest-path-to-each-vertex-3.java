e4a3fbff26eb1147b7350ade010526fee0fb33f4
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-10 03:23:48 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 212ms, Memory: 18MB, Status: Passed - Codetree

--- 250910/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
+++ 250910/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -39,6 +39,10 @@ public class Main {
             }
         }
         for(int i = 2; i<=n; i++){
+            if(dist[i] == 10001){
+                System.out.println(-1);
+                continue;
+            }
             System.out.println(dist[i]);
         }
     }
