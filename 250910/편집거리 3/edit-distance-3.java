75642890a19558f01bc304f34468b05661c206ac
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-10 01:16:25 +0900

    Title: 편집거리 3, Time: 148ms, Memory: 13MB, Status: Wrong Answer - Codetree

--- 250910/편집거리 3/edit-distance-3.java
+++ 250910/편집거리 3/edit-distance-3.java
@@ -6,24 +6,26 @@ public class Main {
         String b = sc.next();
         int n = a.length();
         int m = b.length();
-        int [][] dp = new int [n+1][m+1];
 
         a = "!"+a;
         b = "!"+b;
 
-        // 초기화: 한쪽 문자열이 비어있을 때 편집거리
-        for (int i = 0; i <= n; i++) dp[i][0] = i; // 삭제
-        for (int j = 0; j <= m; j++) dp[0][j] = j; // 삽입
+       int[][] dp = new int[n + 1][m + 1];
 
+        // LCS 계산
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (a.charAt(i - 1) == b.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        System.out.print(dp[n][m]);
+
+        int lcsLength = dp[n][m];
+        int editDistance = n + m - 2 * lcsLength; // 삽입/삭제만 고려
+
+        System.out.println(editDistance);
     }
 }
