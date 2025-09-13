f7515b40fe57823861fabb701e84c61ce0ab0f86
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-13 21:49:16 +0900

    Title: 각 정점까지의 최단 경로, Time: 274ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250913/각 정점까지의 최단 경로/shortest-path-to-each-vertex.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+    public static ArrayList<int []>[] graph = new ArrayList[20001];
+    public static PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+    public static int[] dist = new int[20001];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        for(int i = 1; i<=n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            graph[u].add(new int[]{v,w});
+            graph[v].add(new int[]{u, w});
+        }
+        for(int i = 1; i<=n; i++){
+            dist[i] = 100000001;
+        }
+        dist[k] = 0;
+        pq.add(new int []{0,k});
+
+        while(!pq.isEmpty()){
+            int minDist = pq.peek()[0];
+            int minIdx = pq.peek()[1];
+            pq.poll();
+
+            if(minDist != dist[minIdx]) continue;
+
+            for(int j = 0; j < graph[minIdx].size(); j++){
+                int targetIdx = graph[minIdx].get(j)[0];
+                int targetDist = graph[minIdx].get(j)[1];
+
+                int newDist = dist[minIdx] + targetDist;
+
+                if(dist[targetIdx]>newDist){
+                    dist[targetIdx] = newDist;
+                    pq.add(new int[]{newDist, targetIdx});
+                }
+            }
+        }
+        for(int i = 1; i<=n; i++){
+            
+            System.out.println(dist[i]);
+        }
+    }
+}
