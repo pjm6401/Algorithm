952e58e441ce2ee6ddacd37b277cbc8457862beb
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-15 00:23:59 +0900

    Title: 천 개의 정거장, Time: 2556ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

--- 250915/천 개의 정거장/thousand-stops.java
+++ 250915/천 개의 정거장/thousand-stops.java
@@ -1,116 +1,141 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
-class Pair {
+// 최종 거리(비용, 시간)를 저장하는 클래스
+class ResultPair {
     long cost, time;
 
-    public Pair(long cost, long time) {
+    public ResultPair(long cost, long time) {
         this.cost = cost;
         this.time = time;
     }
 
-    public boolean isGreaterThan(Pair p) {
-        return this.cost > p.cost || (this.cost == p.cost && this.time > p.time);
+    // 현재 Pair가 p보다 더 큰 값인지 비교하는 메서드
+    public boolean isGreaterThan(ResultPair p) {
+        if (this.cost != p.cost) {
+            return this.cost > p.cost;
+        }
+        return this.time > p.time;
+    }
+}
+
+// 그래프의 간선 정보를 담는 클래스
+class Edge {
+    int to;
+    long cost;
+    long time;
+
+    public Edge(int to, long cost, long time) {
+        this.to = to;
+        this.cost = cost;
+        this.time = time;
+    }
+}
+
+// 우선순위 큐에 들어갈 경로 정보를 담는 클래스
+class Path implements Comparable<Path> {
+    int to;
+    long cost;
+    long time;
+
+    public Path(int to, long cost, long time) {
+        this.to = to;
+        this.cost = cost;
+        this.time = time;
+    }
+
+    // 우선순위 큐의 정렬 기준 (비용 우선, 비용이 같으면 시간 우선)
+    @Override
+    public int compareTo(Path other) {
+        if (this.cost != other.cost) {
+            return Long.compare(this.cost, other.cost);
+        }
+        return Long.compare(this.time, other.time);
     }
 }
 
 public class Main {
-    public static final int INT_MAX = Integer.MAX_VALUE;
-    public static final long INF = (long)1e17 + 1;
+    public static final long INF = (long) 1e17 + 1;
     public static final int MAX_M = 1000;
-    
-    // 변수 선언
+
     public static int a, b, n, m = 1000;
-    public static Pair[][] graph = new Pair[MAX_M + 1][MAX_M + 1]; // (비용, 시간)을 기록
-    public static boolean[] visited = new boolean[MAX_M + 1];
-    
-    public static Pair[] dist = new Pair[MAX_M + 1];               // (비용, 시간)을 기록
+    // 인접 리스트로 그래프 표현 변경
+    public static ArrayList<Edge>[] adj = new ArrayList[MAX_M + 1];
+    public static ResultPair[] dist = new ResultPair[MAX_M + 1];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 입력
         a = sc.nextInt();
         b = sc.nextInt();
         n = sc.nextInt();
 
-        // 초기 graph 값에 전부 큰 값 기록
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= m; j++) {
-                graph[i][j] = new Pair(INF, 0);
-            }
-            // 자기 자신은 비용과 시간이 전혀 소요되지 않음
-            graph[i][i] = new Pair(0, 0);
+        // 인접 리스트 및 dist 배열 초기화
+        for (int i = 1; i <= m; i++) {
+            adj[i] = new ArrayList<>();
+            dist[i] = new ResultPair(INF, INF);
         }
 
-        // 그래프를 인접행렬로 표현
-        for(int i = 1; i <= n; i++) {
+        // 그래프 구성 (인접 리스트)
+        for (int i = 1; i <= n; i++) {
             int cost = sc.nextInt();
             int stopNum = sc.nextInt();
-            
+
             ArrayList<Integer> stops = new ArrayList<>();
-            for(int j = 0; j < stopNum; j++) {
-                int x = sc.nextInt();
-                stops.add(x);
+            for (int j = 0; j < stopNum; j++) {
+                stops.add(sc.nextInt());
             }
 
-            for(int j = 0; j < stopNum; j++) {
-                for(int k = j + 1; k < stopNum; k++) {
-                    Pair newP = new Pair(cost, k - j);
-                    if(graph[stops.get(j)][stops.get(k)].isGreaterThan(newP))
-                        graph[stops.get(j)][stops.get(k)] = newP;
+            // 한 노선 내의 모든 정류장 쌍에 대해 간선 추가
+            for (int j = 0; j < stopNum; j++) {
+                for (int k = j + 1; k < stopNum; k++) {
+                    int startNode = stops.get(j);
+                    int endNode = stops.get(k);
+                    long time = k - j;
+                    // 양방향이 아니므로 단방향 간선만 추가
+                    adj[startNode].add(new Edge(endNode, cost, time));
                 }
             }
         }
-        
-        // 그래프에 있는 모든 노드들에 대해
-        // 초기값을 전부 아주 큰 값으로 설정
-        // INT_MAX 그 자체로 설정하면
-        // 후에 덧셈 진행시 overflow가 발생할 수도 있으므로
-        // 적당히 큰 숫자로 적어줘야함에 유의!
-        for(int i = 1; i <= m; i++)
-            dist[i] = new Pair(INF, 0);
 
-        // 시작위치에는 dist값을 0으로 설정
-        dist[a] = new Pair(0, 0);
+        // 시작 위치 설정
+        dist[a] = new ResultPair(0, 0);
 
-        // O(|V|^2) 다익스트라 코드
-        for(int i = 1; i <= m; i++) {
-            // V개의 정점 중 
-            // 아직 방문하지 않은 정점 중
-            // dist값이 가장 작은 정점을 찾아줍니다.
-            int minIndex = -1;
-            for(int j = 1; j <= m; j++) {
-                if(visited[j])
-                    continue;
-                
-                if(minIndex == -1 || dist[minIndex].isGreaterThan(dist[j]))
-                    minIndex = j;
+        // 우선순위 큐를 이용한 다익스트라 알고리즘
+        PriorityQueue<Path> pq = new PriorityQueue<>();
+        pq.add(new Path(a, 0, 0));
+
+        while (!pq.isEmpty()) {
+            Path currentPath = pq.poll();
+            int u = currentPath.to;
+            long currentCost = currentPath.cost;
+            long currentTime = currentPath.time;
+
+            // 이미 더 좋은 경로를 찾았다면 무시
+            if (currentCost > dist[u].cost || (currentCost == dist[u].cost && currentTime > dist[u].time)) {
+                continue;
             }
 
-            // 최솟값에 해당하는 정점에 방문 표시를 진행합니다.
-            visited[minIndex] = true;
+            // 현재 정점과 연결된 모든 간선을 확인
+            for (Edge edge : adj[u]) {
+                int v = edge.to;
+                long edgeCost = edge.cost;
+                long edgeTime = edge.time;
+                
+                ResultPair newPair = new ResultPair(dist[u].cost + edgeCost, dist[u].time + edgeTime);
 
-            long minCost = dist[minIndex].cost;
-            long minTime = dist[minIndex].time;
-
-            // 최솟값에 해당하는 정점에 연결된 간선들을 보며
-            // 시작점으로부터의 최단거리 값을 갱신해줍니다.
-            for(int j = 1; j <= m; j++) {
-                long cost = graph[minIndex][j].cost;
-                long time = graph[minIndex][j].time;
-                Pair newP = new Pair(minCost + cost, minTime + time);
-                if(dist[j].isGreaterThan(newP))
-                    dist[j] = newP;
+                if (dist[v].isGreaterThan(newPair)) {
+                    dist[v] = newPair;
+                    pq.add(new Path(v, dist[v].cost, dist[v].time));
+                }
             }
         }
 
-        // 만약 도달이 불가능하다면 -1 -1이 답이 됩니다.
-        if(dist[b].cost == INF)
-            dist[b] = new Pair(-1, -1);
-        
-        long minCost = dist[b].cost;
-        long minTime = dist[b].time;
-
-        System.out.print(minCost + " " + minTime);
+        // 결과 출력
+        if (dist[b].cost == INF) {
+            System.out.print("-1 -1");
+        } else {
+            System.out.print(dist[b].cost + " " + dist[b].time);
+        }
     }
 }
