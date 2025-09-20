3bcfaff53616b4eb251e8a122a9305c41f85f022
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-21 03:22:52 +0900

    Title: 낮은 지점들, Time: 1296ms, Memory: 42MB, Status: Passed - Codetree

--- 250921/낮은 지점들/lowest-points.java
+++ 250921/낮은 지점들/lowest-points.java
@@ -1,22 +1,27 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         HashMap<Integer,Integer> map = new HashMap<>();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            map.compute(x, (k, v) -> v == null ? y : Math.min(v, y));
+            if (!map.containsKey(x)) {
+                map.put(x, y);
+            } else {
+                map.put(x, Math.min(map.get(x), y));
+            }
         }
+        
+        
+        long[] ans = {0};
 
-        long ans = 0; // int -> long 변경
-        for (int value : map.values()) {
-            ans += value;
-        }
+        map.forEach((key,value) -> {
+    
+            ans[0] += value;
+        });
 
-        System.out.println(ans);
+        System.out.println(ans[0]);
     }
 }
