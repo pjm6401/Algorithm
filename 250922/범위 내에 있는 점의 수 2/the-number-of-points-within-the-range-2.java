b22a161e0b041c0a314807362f2c8e42e13321f6
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-22 03:55:21 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 1626ms, Memory: 52MB, Status: Passed - Codetree

--- 250922/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 250922/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -1,26 +1,73 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        HashMap<Integer,Integer> map = new HashMap<>();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int q = sc.nextInt();
+        int n = sc.nextInt(); // 점 개수
+        int q = sc.nextInt(); // 질의 개수
         int[] points = new int[n];
         for (int i = 0; i < n; i++) {
             points[i] = sc.nextInt();
-            map.put(points[i],1);
         }
-        for (int i = 0; i < q; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int ans = 0;
 
-            for(int j = a; j<=b; j++){
-                if(map.containsKey(j)){
-                    ans+=1;
-                }
-            }
-            System.out.println(ans);
+        // 1. 점들을 정렬
+        Arrays.sort(points);
+
+        // 2. 좌표 압축: 점 위치를 1 ~ N으로 변환
+        // map: 실제 좌표 -> 압축 좌표
+        Map<Integer, Integer> comp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            comp.put(points[i], i + 1); // 1-based index
         }
+
+        // 3. 압축 좌표에 개수 저장 (여기서는 각 점이 1개씩 존재)
+        int[] cnt = new int[n + 1];
+        for (int p : points) {
+            cnt[comp.get(p)]++;
+        }
+
+        // 4. 부분합 배열 생성
+        int[] prefix = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + cnt[i];
+        }
+
+        // 5. 질의 처리
+        for (int i = 0; i < q; i++) {
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+
+            // l 이상인 첫 좌표 idxL, r 이하인 마지막 좌표 idxR 찾기
+            int idxL = lowerBound(points, l) + 1; // 압축 좌표 (1-based)
+            int idxR = upperBound(points, r);     // 압축 좌표 (1-based)
+
+            if (idxL > idxR) {
+                System.out.println(0);
+            } else {
+                System.out.println(prefix[idxR] - prefix[idxL - 1]);
+            }
+        }
+    }
+
+    // lower_bound: 처음으로 key 이상이 나오는 위치
+    static int lowerBound(int[] arr, int key) {
+        int lo = 0, hi = arr.length;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (arr[mid] < key) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo; // 0-based index
+    }
+
+    // upper_bound: 처음으로 key 초과가 나오는 위치
+    static int upperBound(int[] arr, int key) {
+        int lo = 0, hi = arr.length;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (arr[mid] <= key) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo; // 0-based index
     }
 }
