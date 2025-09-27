6c065c718e64777b3ab22e84d34cca038d568316
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-28 01:51:28 +0900

    Title: 가장 가까운 점, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250928/가장 가까운 점/nearest-point.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] points = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            pq.add(new Pair(x,y,x+y));
+        }
+
+        for (int i = 0; i < m; i++) {
+            Pair p = pq.poll();
+
+            p.x += 2;
+            p.y += 2;
+            p.sum +=4;
+            pq.add(p);
+        }
+        Pair p = pq.poll();
+        System.out.println(p.x + " "+p.y);
+        
+    }
+}
+
+class Pair implements Comparable<Pair>{
+    int x;
+    int y;
+    int sum;
+    public Pair(int x, int y,int sum){
+        this.x = x;
+        this.y = y;
+        this.sum = sum;
+    }
+
+    @Override
+    public int compareTo(Pair p){
+        
+        return this.sum - p.sum;
+    }
+}
