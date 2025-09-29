7b67f6122abf7f8f25b2afabb67c78a9bcd8296c
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-30 02:43:30 +0900

    Title: 점 개수 세기 3, Time: 1843ms, Memory: 57MB, Status: Passed - Codetree

--- 250930/점 개수 세기 3/count-number-of-points-3.java
+++ 250930/점 개수 세기 3/count-number-of-points-3.java
@@ -1,25 +1,58 @@
-import java.util.*;
-public class Main {
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.HashMap;
+
+class Pair {
+    int a, b;
+
+    public Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
+
+public class Main {    
+    public static final int MAX_NUM = 100000;
+    
+    // 변수 선언
+    public static int n, q;
+    public static int[] arr = new int[MAX_NUM];
+    public static Pair[] queries = new Pair[MAX_NUM];
+    
+    public static TreeSet<Integer> nums = new TreeSet<>();
+    public static HashMap<Integer, Integer> mapper = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int q = sc.nextInt();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(sc.nextInt(),1);
-        }
-        int[][] queries = new int[q][2];
-        for (int i = 0; i < q; i++) {
+        // 입력:
+        n = sc.nextInt();
+        q = sc.nextInt();
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        for(int i = 0; i < q; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            int [] ans = {0};
-            map.forEach((key,value)->{
-                if(a<= key && key<=b){
-                    ans[0] ++;
-                }
-            });
-            System.out.println(ans[0]);
+            queries[i] = new Pair(a, b);
         }
-        // Please write your code here.
+        for(int i = 0; i < n; i++)
+            nums.add(arr[i]);
+        
+        int cnt = 1;
+        for(Integer num : nums) {
+            mapper.put(num, cnt);
+            cnt++;
+        }
+
+        for(int i = 0; i < q; i++) {
+            int a = queries[i].a;
+            int b = queries[i].b;
+
+            int newA = mapper.get(a);
+            int newB = mapper.get(b);
+
+            System.out.println(newB - newA + 1);
+        }
     }
 }
