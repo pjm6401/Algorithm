ec1de99dbfc9c17e1a9bc377e5866ffbb6a10cea
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-29 23:39:49 +0900

    Title: 앞에서부터 삭제하기 2, Time: 1987ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

--- 250929/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 250929/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -1,30 +1,33 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++)
-           arr[i] = sc.nextInt();
-        double min = 0;
-        for(int k = 1; k<=n-2; k++){
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        double maxAvg = 0.0;
+        
+        for (int k = 1; k <= n - 2; k++) {
             PriorityQueue<Integer> pq = new PriorityQueue<>();
-            for(int i = k; i<n; i++){
-                pq.add(arr[i]);
-            }
-            
-            pq.poll();
             int sum = 0;
-            while(!pq.isEmpty()){
-                int x = pq.poll();
-
-                sum += x;
+            
+            for (int i = k; i < n; i++) {
+                pq.add(arr[i]);
+                sum += arr[i];
             }
 
-            min = Math.max(min,sum/(n-k-1));
-        }
+            int minVal = pq.poll();
+            sum -= minVal;
 
-        System.out.printf("%.2f\n", min);
+            double avg = sum / (double)(pq.size());
+
+            maxAvg = Math.max(maxAvg, avg);
+        }
+        
+        System.out.printf("%.2f\n", maxAvg);
     }
 }
