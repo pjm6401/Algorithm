c38164616b04351ac551f69972f7d0a22b7d52be
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-29 23:40:51 +0900

    Title: 앞에서부터 삭제하기 2, Time: 817ms, Memory: 34MB, Status: Passed - Codetree

--- 250929/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 250929/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -3,31 +3,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
-        
+
+        long[] suffixSum = new long[n + 1];
+        int[] suffixMin = new int[n + 1];
+
+
+        suffixSum[n] = 0;
+        suffixMin[n] = Integer.MAX_VALUE;
+
+  
+        for (int i = n - 1; i >= 0; i--) {
+            suffixSum[i] = suffixSum[i + 1] + arr[i];
+            suffixMin[i] = Math.min(arr[i], suffixMin[i + 1]);
+        }
+
         double maxAvg = 0.0;
-        
+
         for (int k = 1; k <= n - 2; k++) {
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-            int sum = 0;
-            
-            for (int i = k; i < n; i++) {
-                pq.add(arr[i]);
-                sum += arr[i];
-            }
-
-            int minVal = pq.poll();
-            sum -= minVal;
-
-            double avg = sum / (double)(pq.size());
-
+            long sum = suffixSum[k];
+            int minVal = suffixMin[k];
+            double avg = (sum - minVal) / (double)(n - k - 1);
             maxAvg = Math.max(maxAvg, avg);
         }
-        
+
         System.out.printf("%.2f\n", maxAvg);
     }
 }
