bbb07d5ef087786ccec464912fcaf9bb3bba063b
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-09-30 02:57:24 +0900

    Title: 점 개수 세기, Time: 3052ms, Memory: 168MB, Status: Time Limit Exceed - Codetree

---
+++ 250930/점 개수 세기/count-number-of-points.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeSet<Integer> set = new TreeSet<>();
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        int N = sc.nextInt();
+        int Q = sc.nextInt();
+        int[] points = new int[N];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            points[i] = sc.nextInt();
+            set.add(points[i]);
+            min = Math.min(min,points[i]);
+            max = Math.max(max,points[i]);
+        }
+        int cnt = 0;
+        for(int i = min; i<= max; i++){
+            if(set.contains(i)){
+                cnt++;   
+            }
+
+            map.put(i,cnt);
+        }
+
+        int[][] queries = new int[Q][2];
+         for (int i = 0; i < Q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            // TreeMap의 floorKey 사용
+            int keyB = map.floorKey(b) != null ? map.floorKey(b) : Integer.MIN_VALUE;
+            int keyA = map.floorKey(a - 1) != null ? map.floorKey(a - 1) : Integer.MIN_VALUE;
+
+            int countB = keyB == Integer.MIN_VALUE ? 0 : map.get(keyB);
+            int countA = keyA == Integer.MIN_VALUE ? 0 : map.get(keyA);
+
+            System.out.println(countB - countA);
+        }
+    }
+}
