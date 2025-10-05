c4609b1fd3eafe7768606037652e33f6b550f466
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-06 02:59:27 +0900

    Title: 가장 짧은 부분합, Time: 432ms, Memory: 23MB, Status: Passed - Codetree

--- 251006/가장 짧은 부분합/shortest-subtotal.java
+++ 251006/가장 짧은 부분합/shortest-subtotal.java
@@ -1,29 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int s = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
-        for(int i =0; i<n; i++){
-            int sum = 0;
-            int cnt = 0;
-            for(int j = i; j<n; j++){
-                sum += arr[j];
-                cnt ++;
-                if(sum>=s){
-                    min = Math.min(min,cnt);
-                    break;
-                }
+        int left = 0, sum = 0, min = Integer.MAX_VALUE;
+
+        for (int right = 0; right < n; right++) {
+            sum += arr[right]; // 오른쪽 포인터 확장
+
+            // 합이 s 이상이면, 왼쪽을 당기며 최소 길이 갱신
+            while (sum >= s) {
+                min = Math.min(min, right - left + 1);
+                sum -= arr[left++];
             }
-            
         }
-        if(min == Integer.MAX_VALUE) min = -1;
-        System.out.println(min);
+
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
     }
 }
