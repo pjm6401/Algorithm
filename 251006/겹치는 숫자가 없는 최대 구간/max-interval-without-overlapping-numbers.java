6bc89ffb68282ced5478f9eb524336504102e4e3
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-06 03:10:52 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251006/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] visit = new int[100000];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int j = 0;
+        int ans = 0;
+        for (int i = 0; i<n; i++) {
+            visit[arr[i]] ++;
+
+            while(j+1<n && visit[arr[j+1]] != 1){
+                visit[arr[j+1]] ++;
+                j++;
+            }
+
+            ans = Math.max(ans,j-i);
+            
+            visit[arr[i]] --;
+        }
+
+        System.out.println(ans);
+    }
+}
