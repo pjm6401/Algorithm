1537fc7e8a2220f34d7736ec05ef83a2daf4f780
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-07 23:10:44 +0900

    Title: 화재 진압, Time: 1115ms, Memory: 48MB, Status: Passed - Codetree

--- 251007/화재 진압/fire-suppression.java
+++ 251007/화재 진압/fire-suppression.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        TreeSet<Integer> set = new TreeSet<>();
+        //TreeSet<Integer> set = new TreeSet<>();
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] fires = new int[n];
@@ -11,9 +11,22 @@ public class Main {
             fires[i] = sc.nextInt();
         for (int i = 0; i < m; i++){
             stations[i] = sc.nextInt();
-            set.add(stations[i]);
+            //set.add(stations[i]);
         }
         int ans = 0;
+        int j = 0;
+        Arrays.sort(fires);
+        Arrays.sort(stations);
+
+        for(int i = 0 ;i<n; i++){
+            while(j+1<m && dist(fires[i],stations[j]) > dist(fires[i],stations[j+1])) j++;
+
+            ans = Math.max(ans,dist(fires[i],stations[j]));
+        }
+
+
+        System.out.println(ans);
+        /*
         for(int i = 0; i<n; i++){
             Integer min = set.floor(fires[i]);
             Integer max = set.ceiling(fires[i]);
@@ -26,8 +39,11 @@ public class Main {
                 int temp = Math.min(Math.abs(Math.abs(fires[i]) - Math.abs(min)),Math.abs(Math.abs(max) - Math.abs(fires[i])));
                 ans = Math.max(ans,temp);
             }
-        }
-        System.out.println(ans);
-            
+        }*/  
+    }
+
+    public static int dist(int fire, int station){
+        return Math.abs(fire - station);
     }
 }
+
