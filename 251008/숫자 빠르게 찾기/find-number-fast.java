1f2eb64ce419ee0f079c4a0adddf95c1278ae7e7
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-08 00:54:40 +0900

    Title: 숫자 빠르게 찾기, Time: 1589ms, Memory: 41MB, Status: Passed - Codetree

---
+++ 251008/숫자 빠르게 찾기/find-number-fast.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int ans = -1;
+            int left = 0;
+            int right = n-1;
+
+            while(left<=right){
+                 int mid = (left + right) / 2;
+                if(arr[mid] == x){
+                    ans = mid; // 또는 arr[mid]
+                    break;
+                } else if(arr[mid] > x){
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            if(ans != -1) ans++;
+            System.out.println(ans);
+        }
+    }
+}
