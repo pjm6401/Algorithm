7d57006ae406336064bcd085bb9205d73ce01323
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-08 01:26:46 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251008/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long m = sc.nextLong();
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(long i = a; i<=b; i++){
+            long x = i;
+            int cnt = 0;
+            long left = 1;
+            long right = m;
+
+            while(left<=right){
+                long mid = (left+right)/2;
+                
+                if(mid == x){
+                    cnt++;
+                    break;
+                }
+
+                if(mid > x){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+                cnt ++;
+            }
+            min = Math.min(min,cnt);
+            max = Math.max(max,cnt);
+
+        }
+        System.out.println(min+" "+max);
+    }
+}
