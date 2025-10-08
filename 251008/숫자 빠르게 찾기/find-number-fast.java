1abb215436505b2b391ef30ddd04d9898f88a4f6
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-08 21:48:07 +0900

    Title: 숫자 빠르게 찾기, Time: 1972ms, Memory: 51MB, Status: Passed - Codetree

--- 251008/숫자 빠르게 찾기/find-number-fast.java
+++ 251008/숫자 빠르게 찾기/find-number-fast.java
@@ -5,6 +5,7 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] arr = new int[n];
+        //
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
         for (int i = 0; i < m; i++) {
