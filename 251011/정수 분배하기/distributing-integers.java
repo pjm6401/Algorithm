98d745ac6175e3b1a05efc5de7769522041528b7
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-11 23:51:29 +0900

    Title: 정수 분배하기, Time: 176ms, Memory: 11MB, Status: Passed - Codetree

--- 251011/정수 분배하기/distributing-integers.java
+++ 251011/정수 분배하기/distributing-integers.java
@@ -31,9 +31,9 @@ public class Main {
         int count = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= n) {
-    count += arr[i] / n;
-}
+            
+                count += arr[i] / n;
+            
         }
 
 
