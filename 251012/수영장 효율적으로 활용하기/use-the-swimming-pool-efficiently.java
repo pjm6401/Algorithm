415dca62f2642fae31f542ded61cada1c1a9c95e
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-12 03:50:14 +0900

    Title: 수영장 효율적으로 활용하기, Time: 562ms, Memory: 24MB, Status: Passed - Codetree

--- 251012/수영장 효율적으로 활용하기/use-the-swimming-pool-efficiently.java
+++ 251012/수영장 효율적으로 활용하기/use-the-swimming-pool-efficiently.java
@@ -1,43 +1,45 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] arr = new int[n];
+        long sumArr = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
+            sumArr += arr[i];
         }
-        long left = 1;
-        long right = Long.MAX_VALUE-1;
-        long time = Long.MAX_VALUE;
-        while(left<=right){
-            long mid = (left+right)/2;
 
-            if(isPossible(mid,arr,m)){
-                time = Math.min(time,mid);
-                right = mid-1;
-                
-            }else{
-                left = mid +1;
+        long left = 1;
+        long right = sumArr; // 최대합으로 설정
+        long answer = sumArr;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            if (isPossible(mid, arr, m)) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        System.out.println(time);
+
+        System.out.println(answer);
     }
 
-
-    public static boolean isPossible(long mid, int [] arr, int m){
-        int lain = 1;
-        int sum = 0;
-        for(int i = 0; i<arr.length; i++){
-            sum+=arr[i];
-
-            if(sum>mid){
-                sum = arr[i];
-                lain++;
+    public static boolean isPossible(long mid, int[] arr, int m) {
+        int count = 1;
+        long sum = 0;
+        for (int num : arr) {
+            if (num > mid) return false;
+            sum += num;
+            if (sum > mid) {
+                sum = num;
+                count++;
             }
         }
-
-        return lain<=m;
+        return count <= m;
     }
 }
