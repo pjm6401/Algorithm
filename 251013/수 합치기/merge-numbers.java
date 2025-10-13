67f25b6bcfbf8966bfc75e14bc3d6d147eba643a
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-13 23:55:13 +0900

    Title: 수 합치기, Time: 871ms, Memory: 29MB, Status: Passed - Codetree

---
+++ 251013/수 합치기/merge-numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            pq.add(sc.nextInt());
+        }
+        int sum = 0;
+        while(pq.size()>1){
+            int a = pq.poll();
+            int b = pq.poll();
+            pq.add(a+b);
+            sum += a+b;
+        }
+        System.out.println(sum);
+    }
+}
