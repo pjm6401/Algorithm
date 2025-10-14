8c272a987b16fe7a0aaff3700790dfa8c36b884a
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-14 23:06:01 +0900

    Title: 수 채우기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251014/수 채우기/fill-in-number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int limit = n/2;
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i<=limit; i++){
+            int last = n - (2*i);
+
+            int cnt = last/5;
+
+            last -= last/5 * 5;
+
+            if(last == 0){
+                min = Math.min(min,i+cnt);
+            }
+            
+        }
+
+        if(min == Integer.MAX_VALUE) min = -1;
+
+        System.out.println(min);
+    }
+}
