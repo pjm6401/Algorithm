15801fedfc61f3a670c0e6cc141a65334b2e69df
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-15 22:51:21 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 900ms, Memory: 39MB, Status: Passed - Codetree

--- 251015/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
+++ 251015/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
@@ -9,19 +9,21 @@ public class Main {
         for(int i = 1; i<=2*n; i++){
             set.add(i);
         }
-        
+        for (int i = 0; i < n; i++) {
+            bCards[i] = sc.nextInt();
+            set.remove(bCards[i]);
+        }
+            
 
         int win = 0;
         for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();
+            int x = bCards[i];
             if(set.higher(x) != null){
                 int y = set.higher(x);
-                set.remove(x);
                 set.remove(y);
                 win++;
             }else{
                int y = set.first();
-               set.remove(x);
                set.remove(y);
             }
         }
