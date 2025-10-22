c78beb9f11a3e85477be30a689ed418a3ed4fc4e
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-10-23 00:46:50 +0900

    Title: 자연수 M/2개의 쌍, Time: 143ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251023/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 251023/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -2,27 +2,64 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Long> list = new ArrayList<>();
+        List<Pair> list = new ArrayList<>();
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            long x = sc.nextInt();
-            long y = sc.nextInt();
-            for(int j = 0; j<x; j++){
-                list.add(y);
-            }
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            list.add(new Pair(y,x));
         }
         Collections.sort(list);
 
-        long max = 0;
+        int max = 0;
+        int start = 0;
+        int last = list.size()-1;
+        int startIdx = 0;
+        int lastIdx = 0;
+        boolean flag = true;
+        
+        while(flag){
+            Pair sPair = list.get(start);
+            Pair ePair = list.get(last);
+            
+            if(start>=list.size()/2 || last<list.size()/2) break;
 
-        for(int i = 0 ; i<list.size()/2; i++){
-            if(max <= list.get(i) + list.get(list.size()-1-i)){
-                max = list.get(i) + list.get(list.size()-1-i);
+            if(sPair.num+ePair.num >=max){
+                max = sPair.num+ePair.num;
             }else{
                 break;
             }
+
+
+            if(startIdx+sPair.count>lastIdx+ePair.count){
+                last--;
+                lastIdx+=ePair.count;
+            }else if(startIdx+sPair.count<lastIdx+ePair.count){
+                start++;
+                startIdx+=sPair.count;
+            }else{
+                start++;
+                last--;
+            }
+
+
         }
 
         System.out.println(max);
+    }
+}
+
+class Pair implements Comparable<Pair>{
+    int num;
+    int count;
+
+    public Pair(int num, int count){
+        this.num = num;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(Pair p){
+        return this.num - p.num;
     }
 }
