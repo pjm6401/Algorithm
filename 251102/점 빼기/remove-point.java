f1b59acd30615c93bf56e5517e64f4a7081b676a
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-11-02 00:19:56 +0900

    Title: 점 빼기, Time: 1415ms, Memory: 25MB, Status: Passed - Codetree

--- 251102/점 빼기/remove-point.java
+++ 251102/점 빼기/remove-point.java
@@ -1,38 +1,41 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        TreeSet<Integer> [] set = new TreeSet [100001];
-        TreeSet<Integer> xSet = new TreeSet<>();
-        for(int i = 1; i<100001; i++){
-            set[i] = new TreeSet<>();
-        }
 
-        for(int i = 0; i<n; i++){
-            int x= sc.nextInt();
-            int y= sc.nextInt();
-            set[x].add(y);
+        Map<Integer, TreeSet<Integer>> map = new TreeMap<>();
+        TreeSet<Integer> xSet = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            map.putIfAbsent(x, new TreeSet<>());
+            map.get(x).add(y);
             xSet.add(x);
         }
 
-        for(int i = 0; i<m; i++){
+        for (int i = 0; i < m; i++) {
             int x = sc.nextInt();
             Integer targetX = xSet.ceiling(x);
 
-            if(targetX == null){
-                System.out.println(-1 + " "+ -1);
-            }else if (set[targetX].size()>1){
-                int targetY = set[targetX].first();
-                System.out.println(targetX + " "+targetY);
-                set[targetX].remove(targetY);
+            if (targetX == null) {
+                System.out.println("-1 -1");
+                continue;
             }
-            else{
-                int targetY = set[targetX].first();
-                System.out.println(targetX + " "+targetY);
+
+            TreeSet<Integer> ys = map.get(targetX);
+            int targetY = ys.first();
+            System.out.println(targetX + " " + targetY);
+            ys.remove(targetY);
+
+            // y가 다 사라졌다면 x 자체 삭제
+            if (ys.isEmpty()) {
                 xSet.remove(targetX);
-                set[targetX].remove(targetY);
+                map.remove(targetX);
             }
         }
     }
