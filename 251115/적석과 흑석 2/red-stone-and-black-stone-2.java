c9ac6ee8bbc6ce5ccb48906a488550471d190259
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-11-15 00:39:49 +0900

    Title: 적석과 흑석 2, Time: 1530ms, Memory: 57MB, Status: Wrong Answer - Codetree

--- 251115/적석과 흑석 2/red-stone-and-black-stone-2.java
+++ 251115/적석과 흑석 2/red-stone-and-black-stone-2.java
@@ -1,56 +1,41 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        TreeSet<Integer> set = new TreeSet<>();
-        List<Pair> list = new ArrayList<>();
+
         int c = sc.nextInt();
         int n = sc.nextInt();
-        int[] redStones = new int[c];
+
+        TreeSet<Integer> set = new TreeSet<>();
         for (int i = 0; i < c; i++) {
             set.add(sc.nextInt());
         }
-        int[][] blackStones = new int[n][2];
+
+        List<Pair> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            list.add(new Pair(s,e));
-
+            list.add(new Pair(sc.nextInt(), sc.nextInt()));
         }
-        Collections.sort(list);
-        int cnt = 0;
-        for(int i = 0; i<list.size(); i++){
-            Pair p = list.get(i);
-            
-            while(!set.isEmpty()){
-                int k = set.first();
-                
-                if(isRanage(p.s, p.e, k)){
-                    cnt++;
-                    set.remove(k);
-                    break;
-                }
 
-                if(k<p.s){
-                    set.remove(k);
-                }else if(k>p.e){
-                    break;
-                }
+        Collections.sort(list);
+
+        int cnt = 0;
+
+        for (Pair p : list) {
+            Integer k = set.ceiling(p.s);  
+
+            if (k != null && k <= p.e) {   
+                cnt++;
+                set.remove(k);
             }
-            if(set.isEmpty()) break;
         }
 
         System.out.println(cnt);
     }
-
-    public static boolean isRanage(int s, int e, int k){
-        return (s<=k && k<=e);
-    }
 }
 
-
-class Pair implements Comparable<Pair>{
-    int s,e;
+class Pair implements Comparable<Pair> {
+    int s, e;
 
     public Pair(int s, int e){
         this.s = s;
@@ -59,9 +44,7 @@ class Pair implements Comparable<Pair>{
 
     @Override
     public int compareTo(Pair p){
-        if(this.s == p.s){
-            return this.e - p.e;
-        }
-        return this.s - p.s;
+        if (this.s != p.s) return this.s - p.s;
+        return this.e - p.e;
     }
 }
