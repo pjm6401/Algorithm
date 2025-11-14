3ff7452b654df3415b08ec8777ff7e995a1f2e1f
Min_98 <40224148+pjm6401@users.noreply.github.com>
2025-11-14 23:22:16 +0900

    Title: 정수 N개의 합 2, Time: 761ms, Memory: 35MB, Status: Passed - Codetree

--- 251114/정수 N개의 합 2/sum-of-n-integers-2.java
+++ 251114/정수 N개의 합 2/sum-of-n-integers-2.java
@@ -1,23 +1,38 @@
 import java.util.Scanner;
-public class Main {
+
+public class Main {    
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, k;
+    public static int[] arr = new int[MAX_N + 1];
+    public static int[] prefixSum = new int[MAX_N + 1];
+    public static int ans = INT_MIN;
+    
+    // [s, e] 구간 내의 원소의 합을 반환합니다.
+    public static int getSum(int s, int e) {
+        return prefixSum[e] - prefixSum[s - 1];
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        // 입력:
+        n = sc.nextInt();
+        k = sc.nextInt();
+        for(int i = 1; i <= n; i++)
             arr[i] = sc.nextInt();
-        }
-        int [] temp = new int [n+1];
 
-        for(int i = 1; i<n; i++){
-            temp[i] = temp[i-1] + arr[i];
-        }
-
+        // 누적합 배열을 만들어줍니다.
+        prefixSum[0] = 0;
+        for(int i = 1; i<= n; i++)
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        
+        // 모든 구간에 대해 합을 찾아
+        // 그 중 최댓값을 갱신합니다.
         for(int i = 1; i <= n - k + 1; i++)
             ans = Math.max(ans, getSum(i, i + k - 1));
 
         System.out.print(ans);
     }
-    
 }
